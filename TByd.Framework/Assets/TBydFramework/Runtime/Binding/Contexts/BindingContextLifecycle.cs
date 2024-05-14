d5485872841ce9c6549308c84ef9c1e4c7eee0fb
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:36:09 +0800

    Create BindingContextLifecycle.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Contexts/BindingContextLifecycle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace TBydFramework.Runtime.Binding.Contexts
+{
+    public class BindingContextLifecycle : MonoBehaviour
+    {
+        private IBindingContext bindingContext;
+        public IBindingContext BindingContext
+        {
+            get { return this.bindingContext; }
+            set
+            {
+                if (this.bindingContext == value)
+                    return;
+
+                if (this.bindingContext != null)
+                    this.bindingContext.Dispose();
+
+                this.bindingContext = value;
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (this.bindingContext != null)
+            {
+                this.bindingContext.Dispose();
+                this.bindingContext = null;
+            }
+        }
+    }
+}
