8ee96ccef0077ae804a2ec1d2f439ec1b8eb97b3
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:18:36 +0800

    Create SingletonProperty.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Singleton/SingletonProperty.cs
@@ -0,0 +1,46 @@
+﻿namespace TBydFramework.Runtime.Singleton
+{
+    /// <summary>
+    /// 单例属性
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class SingletonProperty<T> where T : class, ISingleton
+    {
+        /// <summary>
+        /// 静态实例
+        /// </summary>
+        private static T _instance;
+
+        /// <summary>
+        /// 标签锁
+        /// </summary>
+        private static readonly object _lock = new object();
+
+        /// <summary>
+        /// 静态属性
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = SingletonCreator.CreateSingleton<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// 资源释放
+        /// </summary>
+        public static void Dispose()
+        {
+            _instance = null;
+        }
+    }
+}
