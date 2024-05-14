758bd68cdc3132cdf9e4690b9c853d7ee44cabe6
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:54:56 +0800

    Create ICoroutinePromise.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Execution/ICoroutinePromise.cs
@@ -0,0 +1,22 @@
+﻿using TBydFramework.Runtime.Asynchronous;
+using UnityEngine;
+
+namespace TBydFramework.Runtime.Execution
+{
+    public interface ICoroutinePromise : Asynchronous.IPromise
+    {
+        void AddCoroutine(Coroutine coroutine);
+    }
+
+    public interface ICoroutinePromise<TResult> : Asynchronous.IPromise<TResult>, ICoroutinePromise
+    {
+    }
+
+    public interface ICoroutineProgressPromise<TProgress> : IProgressPromise<TProgress>, ICoroutinePromise
+    {
+    }
+
+    public interface ICoroutineProgressPromise<TProgress, TResult> : IProgressPromise<TProgress, TResult>, ICoroutineProgressPromise<TProgress>
+    {
+    }
+}
