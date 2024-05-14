4399f186a0a7876a7b510c51210a28d53c5e051c
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:28:44 +0800

    Create UIViewLocatorBase.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/Locators/UIViewLocatorBase.cs
@@ -0,0 +1,50 @@
+﻿using TBydFramework.Runtime.Asynchronous;
+
+namespace TBydFramework.Runtime.Views.Locators
+{
+    public abstract class UIViewLocatorBase : IUIViewLocator
+    {
+        public virtual IView LoadView(string name)
+        {
+            return LoadView<IView>(name);
+        }
+
+        public abstract T LoadView<T>(string name) where T : IView;
+
+        public virtual IWindow LoadWindow(string name)
+        {
+            return LoadWindow<Window>(name);
+        }
+
+        public abstract T LoadWindow<T>(string name) where T : IWindow;
+
+        public virtual IWindow LoadWindow(IWindowManager windowManager, string name)
+        {
+            return LoadWindow<IWindow>(windowManager, name);
+        }
+
+        public abstract T LoadWindow<T>(IWindowManager windowManager, string name) where T : IWindow;
+
+
+        public virtual IProgressResult<float, IView> LoadViewAsync(string name)
+        {
+            return LoadViewAsync<IView>(name);
+        }
+
+        public abstract IProgressResult<float, T> LoadViewAsync<T>(string name) where T : IView;
+
+        public virtual IProgressResult<float, IWindow> LoadWindowAsync(string name)
+        {
+            return LoadWindowAsync<IWindow>(name);
+        }
+
+        public abstract IProgressResult<float, T> LoadWindowAsync<T>(string name) where T : IWindow;
+
+        public virtual IProgressResult<float, IWindow> LoadWindowAsync(IWindowManager windowManager, string name)
+        {
+            return LoadWindowAsync<IWindow>(windowManager, name);
+        }
+
+        public abstract IProgressResult<float, T> LoadWindowAsync<T>(IWindowManager windowManager, string name) where T : IWindow;
+    }
+}
