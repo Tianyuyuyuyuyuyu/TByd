e8cc98ae3b9300b63efa4f966183d42abb617d3c
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 20:37:16 +0800

    Create LiteDBRepository.cs

---
+++ TByd.Framework.Data/Assets/Samples/TByd Framework Data/0.0.1/Examples/Scripts/Repositories/LiteDBRepository.cs
@@ -0,0 +1,23 @@
+﻿#if LITEDB
+using LiteDB;
+
+namespace TBydFramework.Examples.Repositories
+{
+    public abstract class LiteDBRepository<T>
+    {
+        protected ILiteDatabase database;
+        protected ILiteCollection<T> collection;
+        public LiteDBRepository(ILiteDatabase database)
+        {
+            this.database = database;
+        }
+
+        protected ILiteCollection<T> GetCollection()
+        {
+            if (collection == null)
+                collection = database.GetCollection<T>();
+            return collection;
+        }
+    }
+}
+#endif
