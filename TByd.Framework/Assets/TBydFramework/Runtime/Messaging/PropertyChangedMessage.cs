8208c90c05aa6704c8d1ffa90aaa3f9f13d89efc
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:04:10 +0800

    Create PropertyChangedMessage.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Messaging/PropertyChangedMessage.cs
@@ -0,0 +1,22 @@
+﻿namespace TBydFramework.Runtime.Messaging
+{
+    public class PropertyChangedMessage<T> : MessageBase
+    {
+        public PropertyChangedMessage(T oldValue, T newValue, string propertyName) : this(null, oldValue, newValue, propertyName)
+        {
+        }
+
+        public PropertyChangedMessage(object sender, T oldValue, T newValue, string propertyName) : base(sender)
+        {
+            this.PropertyName = propertyName;
+            this.OldValue = oldValue;
+            this.NewValue = newValue;
+        }
+
+        public string PropertyName { get; private set; }
+
+        public T OldValue { get; private set; }
+
+        public T NewValue { get; private set; }
+    }
+}
