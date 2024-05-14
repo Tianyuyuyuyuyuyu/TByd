45233c9aff6e5c001d16dde0bec7ac1b0e8bb544
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 20:35:01 +0800

    Create DataExample.cs

---
+++ TByd.Framework.Data/Assets/Samples/TByd Framework Data/0.0.1/Examples/Scripts/DataExample.cs
@@ -0,0 +1,61 @@
+﻿using TBydFramework.Examples.Repositories;
+using System.IO;
+using UnityEngine;
+#if LITEDB
+using LiteDB;
+#endif
+
+public class DataExample : MonoBehaviour
+{
+    void Start()
+    {
+#if NEWTONSOFT
+        LoadFromJson();
+#endif
+
+#if LITEDB
+        LoadFromLiteDB();
+#endif
+    }
+
+#if NEWTONSOFT
+    void LoadFromJson()
+    {
+        JsonEquipmentInfoRepository equipmentInfoRepository = new JsonEquipmentInfoRepository();
+
+        var e1 = equipmentInfoRepository.GetById(1);
+        Debug.LogFormat("LoadFromJson  id:{0} quality:{1} health:{2}", e1.Id, e1.Quality, e1.Health);
+        var e2 = equipmentInfoRepository.GetBySign("equip001", 4);
+        Debug.LogFormat("LoadFromJson  id:{0} quality:{1} health:{2}", e2.Id, e2.Quality, e2.Health);
+    }
+#endif
+
+#if LITEDB
+    private LiteDatabase database;
+
+    private LiteDatabase LoadDatabase()
+    {
+        TextAsset textAsset = Resources.Load<TextAsset>("LiteDB/data");
+        return new LiteDatabase(new MemoryStream(textAsset.bytes));
+    }
+
+    private void OnDestroy()
+    {
+        if (database != null)
+            database.Dispose();
+    }
+
+    void LoadFromLiteDB()
+    {
+        if (database == null)
+            database = LoadDatabase();
+
+        LiteDBEquipmentInfoRepository equipmentInfoRepository = new LiteDBEquipmentInfoRepository(database);
+
+        var e1 = equipmentInfoRepository.GetById(1);
+        Debug.LogFormat("LoadFromLiteDB id:{0} quality:{1} health:{2}", e1.Id, e1.Quality, e1.Health);
+        var e2 = equipmentInfoRepository.GetBySign("equip001", 4);
+        Debug.LogFormat("LoadFromLiteDB id:{0} quality:{1} health:{2}", e2.Id, e2.Quality, e2.Health);
+    }
+#endif
+}
