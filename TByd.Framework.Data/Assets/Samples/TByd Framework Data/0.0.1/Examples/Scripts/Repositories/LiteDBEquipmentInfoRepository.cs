28405db052bc878fe94718e0479e64ab2384f29d
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 20:37:07 +0800

    Create LiteDBEquipmentInfoRepository.cs

---
+++ TByd.Framework.Data/Assets/Samples/TByd Framework Data/0.0.1/Examples/Scripts/Repositories/LiteDBEquipmentInfoRepository.cs
@@ -0,0 +1,25 @@
+﻿#if LITEDB
+using LiteDB;
+using TBydFramework.Examples.Domains;
+
+namespace TBydFramework.Examples.Repositories
+{
+    public class LiteDBEquipmentInfoRepository : LiteDBRepository<EquipmentInfo>, IEquipmentInfoRepository
+    {
+        public LiteDBEquipmentInfoRepository(ILiteDatabase database) : base(database)
+        {
+        }
+
+
+        public EquipmentInfo GetById(int id)
+        {
+            return GetCollection().FindById(id);
+        }
+
+        public EquipmentInfo GetBySign(string sign, int quality)
+        {
+            return GetCollection().FindOne(c => c.Sign.Equals(sign) && c.Quality == quality);
+        }
+    }
+}
+#endif
