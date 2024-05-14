162623ea88b23973bedf6dd21df3e2d3cf119b74
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 19:49:32 +0800

    Create CodecException.cs

---
+++ TByd.Framework.Connection/Assets/TBydFramework/Connection/Runtime/Codec/CodecException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TBydFramework.Connection.Runtime.Codec
+{
+    public class CodecException : Exception
+    {
+        public CodecException()
+        {
+        }
+
+        public CodecException(string message) : base(message)
+        {
+        }
+
+        public CodecException(string message, Exception exception) : base(message, exception)
+        {
+        }
+
+        public CodecException(string format, params object[] arguments) : base(string.Format(format, arguments))
+        {
+        }
+
+        public CodecException(Exception exception, string format, params object[] arguments) : base(string.Format(format, arguments), exception)
+        {
+        }
+    }
+}
