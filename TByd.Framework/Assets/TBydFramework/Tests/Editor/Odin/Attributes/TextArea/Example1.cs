ad22fe5775fce397c0e5f59f45e7f04c4fbe4d10
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:55:13 +0800

    Create Example1.cs

---
+++ TByd.Framework/Assets/TBydFramework/Tests/Editor/Odin/Attributes/TextArea/Example1.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace TBydFramework.Tests.Editor.Odin.Attributes.TextArea
+{
+    public class Example1 : MonoBehaviour
+    {
+        [TextArea]
+        public string content = "";
+    }
+}
