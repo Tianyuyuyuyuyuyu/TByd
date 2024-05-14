868e14f83b233ada4036dcdf8a1f554f169836b8
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:06:30 +0800

    Create IObjectPool.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/ObjectPool/IObjectPool.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.ObjectPool
+{
+    public interface IObjectPool : IDisposable
+    {
+        /// <summary>
+        /// Gets an object from the pool if one is available, otherwise creates one.
+        /// </summary>
+        /// <returns></returns>
+        object Allocate();
+
+        /// <summary>
+        /// Return an object to the pool,if the number of objects in the pool is greater than or equal to the maximum value, the object is destroyed.
+        /// </summary>
+        /// <param name="obj"></param>
+        void Free(object obj);
+    }
+
+    public interface IObjectPool<T> : IObjectPool, IDisposable where T : class
+    {
+        /// <summary>
+        /// Gets an object from the pool if one is available, otherwise creates one.
+        /// </summary>
+        /// <returns></returns>
+        new T Allocate();
+
+        /// <summary>
+        /// Return an object to the pool,if the number of objects in the pool is greater than or equal to the maximum value, the object is destroyed.
+        /// </summary>
+        /// <param name="obj"></param>
+        void Free(T obj);
+    }
+}
