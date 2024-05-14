f21b361731259af61d233533bac8dead80aa2c83
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:41:51 +0800

    Create BindingProxyBase.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/BindingProxyBase.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace TBydFramework.Runtime.Binding.Proxy
+{
+    public abstract class BindingProxyBase : IBindingProxy
+    {
+        #region IDisposable Support
+
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+        ~BindingProxyBase()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
