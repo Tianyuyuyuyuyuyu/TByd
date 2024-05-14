cb91dc0b1215504cad1bd568efc329d0e45da77e
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 20:37:25 +0800

    Create LiteDBUserInfoRepository.cs

---
+++ TByd.Framework.Data/Assets/Samples/TByd Framework Data/0.0.1/Examples/Scripts/Repositories/LiteDBUserInfoRepository.cs
@@ -0,0 +1,24 @@
+﻿#if LITEDB
+using LiteDB;
+using TBydFramework.Examples.Domains;
+
+namespace TBydFramework.Examples.Repositories
+{
+    public class LiteDBUserInfoRepository : LiteDBRepository<UserInfo>, IUserInfoRepository
+    {
+        public LiteDBUserInfoRepository(ILiteDatabase database) : base(database)
+        {
+        }
+
+        public UserInfo GetById(int id)
+        {
+            return GetCollection().FindById(id);
+        }
+
+        public UserInfo GetByUsername(string username)
+        {
+            return GetCollection().FindOne(c => c.Username.Equals(username));
+        }
+    }
+}
+#endif
