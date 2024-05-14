f61877cf1ba6f4382bcc6cc03ca2c25fbda8cc05
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:57:37 +0800

    Create Example1.cs

---
+++ TByd.Framework/Assets/TBydFramework/Tests/Editor/Odin/Attributes/VerticalGroup/Example1.cs
@@ -0,0 +1,34 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace TBydFramework.Tests.Editor.Odin.Attributes.VerticalGroup
+{
+    public class Example1 : MonoBehaviour
+    {
+        [HorizontalGroup("Split")]
+        [VerticalGroup("Split/Left")]
+        public InfoMessageType First;
+
+        [VerticalGroup("Split/Left")]
+        public InfoMessageType Second;
+
+        [VerticalGroup("Split/Right")]
+        public int A;
+
+        [VerticalGroup("Split/Right")]
+        public int B;
+
+        [TitleGroup("Multiple Stacked Boxes")]
+        [HorizontalGroup("Multiple Stacked Boxes/Split")]
+        [VerticalGroup("Multiple Stacked Boxes/Split/Left")]
+        [BoxGroup("Multiple Stacked Boxes/Split/Left/Box A")]
+        public int BoxA;
+
+        [BoxGroup("Multiple Stacked Boxes/Split/Left/Box B")]
+        public int BoxB;
+
+        [VerticalGroup("Multiple Stacked Boxes/Split/Right")]
+        [BoxGroup("Multiple Stacked Boxes/Split/Right/Box C")]
+        public int BoxC, BoxD, BoxE;
+    }
+}
