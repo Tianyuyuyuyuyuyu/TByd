56543f0d0d225d8d4fd023bd264831e14202fe11
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:45:31 +0800

    Create ObservableLiteralSourceProxy.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Sources/Text/ObservableLiteralSourceProxy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using TBydFramework.Runtime.Observables;
+
+namespace TBydFramework.Runtime.Binding.Proxy.Sources.Text
+{
+    public class ObservableLiteralSourceProxy : NotifiableSourceProxyBase, ISourceProxy, IObtainable
+    {
+        private IObservableProperty observableProperty;
+
+        public ObservableLiteralSourceProxy(IObservableProperty source) : base(source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            this.observableProperty = source;
+            this.observableProperty.ValueChanged += OnValueChanged;
+        }
+
+        private void OnValueChanged(object sender, EventArgs e)
+        {
+            this.RaiseValueChanged();
+        }
+
+        public override Type Type { get { return observableProperty.Type; } }
+
+        public virtual object GetValue()
+        {
+            return this.observableProperty.Value;
+        }
+
+        public virtual TValue GetValue<TValue>()
+        {
+            return (TValue)Convert.ChangeType(this.observableProperty.Value, typeof(TValue));
+        }
+
+        #region IDisposable Support    
+        private bool disposedValue = false;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (this.observableProperty != null)
+                    this.observableProperty.ValueChanged -= OnValueChanged;
+
+                disposedValue = true;
+                base.Dispose(disposing);
+            }
+        }
+        #endregion
+    }
+}
