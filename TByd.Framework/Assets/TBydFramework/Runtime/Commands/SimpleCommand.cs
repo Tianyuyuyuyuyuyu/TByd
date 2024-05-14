ebc491b6fc985b4448c3741d3c93e31b537e2cc7
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:52:39 +0800

    Create SimpleCommand.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Commands/SimpleCommand.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Commands
+{
+    public class SimpleCommand : CommandBase
+    {
+        private bool enabled = true;
+        private readonly Action execute;
+
+        public SimpleCommand(Action execute, bool keepStrongRef = false)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            this.execute = keepStrongRef ? execute : execute.AsWeak();
+        }
+
+        public bool Enabled
+        {
+            get { return this.enabled; }
+            set
+            {
+                if (this.enabled == value)
+                    return;
+
+                this.enabled = value;
+                this.RaiseCanExecuteChanged();
+            }
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return this.Enabled;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (this.CanExecute(parameter) && this.execute != null)
+                this.execute();
+        }
+    }
+
+    public class SimpleCommand<T> : CommandBase, ICommand<T>
+    {
+        private bool enabled = true;
+        private readonly Action<T> execute;
+
+        public SimpleCommand(Action<T> execute, bool keepStrongRef = false)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            this.execute = keepStrongRef ? execute : execute.AsWeak();
+        }
+
+        public bool Enabled
+        {
+            get { return this.enabled; }
+            set
+            {
+                if (this.enabled == value)
+                    return;
+
+                this.enabled = value;
+                this.RaiseCanExecuteChanged();
+            }
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return this.Enabled;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (this.CanExecute(parameter) && this.execute != null)
+                this.execute((T)Convert.ChangeType(parameter, typeof(T)));
+        }
+
+        public bool CanExecute(T parameter)
+        {
+            return this.Enabled;
+        }
+
+        public void Execute(T parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
+
