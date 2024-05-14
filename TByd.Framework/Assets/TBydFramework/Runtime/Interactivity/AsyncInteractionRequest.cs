10e4c0ac9aa7d8dcb804116ccdb5494b87fc529e
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:56:10 +0800

    Create AsyncInteractionRequest.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Interactivity/AsyncInteractionRequest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TBydFramework.Runtime.Interactivity
+{
+    /// <summary>
+    /// Implementation of the <see cref="IInteractionRequest"/> interface.
+    /// </summary>
+    public class AsyncInteractionRequest : IInteractionRequest
+    {
+        private object sender;
+
+        public AsyncInteractionRequest() : this(null)
+        {
+        }
+
+        public AsyncInteractionRequest(object sender)
+        {
+            this.sender = sender != null ? sender : this;
+        }
+
+        /// <summary>
+        /// Fired when interaction is needed.
+        /// </summary>
+        public event EventHandler<InteractionEventArgs> Raised;
+
+        /// <summary>
+        /// Fires the Raised event.
+        /// </summary>
+        public Task Raise()
+        {
+            TaskCompletionSource<object> source = new TaskCompletionSource<object>();
+            this.Raised?.Invoke(this.sender, new AsyncInteractionEventArgs(source, null));
+            return source.Task;
+        }
+    }
+
+    /// <summary>
+    /// Implementation of the <see cref="IInteractionRequest"/> interface.
+    /// </summary>
+    public class AsyncInteractionRequest<T> : IInteractionRequest
+    {
+        private object sender;
+        public AsyncInteractionRequest() : this(null)
+        {
+        }
+
+        public AsyncInteractionRequest(object sender)
+        {
+            this.sender = sender != null ? sender : this;
+        }
+
+        /// <summary>
+        /// Fired when interaction is needed.
+        /// </summary>
+        public event EventHandler<InteractionEventArgs> Raised;
+
+        /// <summary>
+        /// Fires the Raised event.
+        /// </summary>
+        /// <param name="context">The context for the interaction request.</param>
+        public async Task<T> Raise(T context)
+        {
+            TaskCompletionSource<object> source = new TaskCompletionSource<object>();
+            this.Raised?.Invoke(this.sender, new AsyncInteractionEventArgs(source, context));
+            await source.Task;
+            return context;
+        }
+    }
+}
