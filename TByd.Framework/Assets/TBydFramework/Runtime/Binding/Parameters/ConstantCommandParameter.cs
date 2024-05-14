eb7cb26a7639d8f2ecc8b27495e16a27130545fa
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:39:51 +0800

    Create ConstantCommandParameter.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Parameters/ConstantCommandParameter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Parameters
+{
+    public class ConstantCommandParameter : ICommandParameter
+    {
+        private object parameter;
+        public ConstantCommandParameter(object parameter)
+        {
+            this.parameter = parameter;
+        }
+        public object GetValue()
+        {
+            return parameter;
+        }
+
+        public Type GetValueType()
+        {
+            return parameter != null ? parameter.GetType() : typeof(object);
+        }
+    }
+
+    public class ConstantCommandParameter<T> : ICommandParameter<T>
+    {
+        private T parameter;
+        public ConstantCommandParameter(T parameter)
+        {
+            this.parameter = parameter;
+        }
+        public T GetValue()
+        {
+            return parameter;
+        }
+
+        public Type GetValueType()
+        {
+            return typeof(T);
+        }
+
+        object ICommandParameter.GetValue()
+        {
+            return GetValue();
+        }
+    }
+}
