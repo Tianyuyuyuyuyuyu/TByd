f63b5e4cdc20c4b6e705edb9d1f792ebae58495e
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:59:35 +0800

    Create LocalizedBindingDescription.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Localizations/UI/LocalizedBindingDescription.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TBydFramework.Runtime.Binding;
+using UnityEngine;
+
+namespace TBydFramework.Runtime.Localizations.UI
+{
+    [Serializable]
+    public class LocalizedBindingDescriptionSet
+    {
+        public List<LocalizedBindingDescription> descriptions = new List<LocalizedBindingDescription>();
+    }
+
+    [Serializable]
+    public class LocalizedBindingDescription
+    {
+        [SerializeField]
+        public string TypeName;
+
+        [SerializeField]
+        public string PropertyName;
+
+        [SerializeField]
+        public string Key;
+
+        [SerializeField]
+        public BindingMode Mode = BindingMode.OneWay;
+
+        public override string ToString()
+        {
+            StringBuilder buf = new StringBuilder();
+            buf.Append(TypeName).Append(" ");
+            buf.Append("{binding ").Append(PropertyName);
+            buf.Append(" Key:").Append(Key);
+            buf.Append(" Mode:").Append(Mode);
+            buf.Append(" }");
+            return buf.ToString();
+        }
+    }
+}
