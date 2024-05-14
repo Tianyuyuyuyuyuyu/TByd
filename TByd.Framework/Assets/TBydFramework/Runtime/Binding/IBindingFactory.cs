76d922a58be2289ec62fb47be2f6991d22c54963
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:39:36 +0800

    Create IBindingFactory.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/IBindingFactory.cs
@@ -0,0 +1,9 @@
+﻿using TBydFramework.Runtime.Binding.Contexts;
+
+namespace TBydFramework.Runtime.Binding
+{
+    public interface IBindingFactory
+    {
+        IBinding Create(IBindingContext bindingContext, object source, object target, BindingDescription bindingDescription);
+    }
+}
