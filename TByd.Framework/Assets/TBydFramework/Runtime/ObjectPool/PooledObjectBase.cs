4f09c08f80b9555398fa59db846e469888501be8
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:07:15 +0800

    Create PooledObjectBase.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/ObjectPool/PooledObjectBase.cs
@@ -0,0 +1,16 @@
+﻿namespace TBydFramework.Runtime.ObjectPool
+{
+    public abstract class PooledObjectBase<T> : IPooledObject where T : PooledObjectBase<T>
+    {
+        private IObjectPool<T> pool;
+        public PooledObjectBase(IObjectPool<T> pool)
+        {
+            this.pool = pool;
+        }
+
+        public virtual void Free()
+        {
+            this.pool.Free((T)this);
+        }
+    }
+}
