7a0c6fef1fa28a7229ceb365acf0db2f31461d6d
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:39:30 +0800

    Create IBinding.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/IBinding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TBydFramework.Runtime.Binding.Contexts;
+
+namespace TBydFramework.Runtime.Binding
+{
+    public interface IBinding : IDisposable
+    {
+        IBindingContext BindingContext { get; set; }
+
+        object Target { get; }
+
+        object DataContext { get; set; }
+    }
+}
