301a7ee92cc98047c3cba7c2ed26b94ec2aae858
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:24:58 +0800

    Create IManageable.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/IManageable.cs
@@ -0,0 +1,40 @@
+﻿using TBydFramework.Runtime.Asynchronous;
+
+namespace TBydFramework.Runtime.Views
+{
+    public interface IManageable : IWindow
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ignoreAnimation"></param>
+        /// <returns></returns>
+        IAsyncResult Activate(bool ignoreAnimation);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ignoreAnimation"></param>
+        /// <returns></returns>
+        IAsyncResult Passivate(bool ignoreAnimation);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ignoreAnimation"></param>
+        /// <returns></returns>
+        IAsyncResult DoShow(bool ignoreAnimation = false);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ignoreAnimation"></param>
+        /// <returns></returns>
+        IAsyncResult DoHide(bool ignoreAnimation = false);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void DoDismiss();
+    }
+}
