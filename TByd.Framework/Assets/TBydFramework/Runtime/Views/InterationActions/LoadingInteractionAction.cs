31cc595be9d3443ba7360370875f6d182b72291a
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:26:16 +0800

    Create LoadingInteractionAction.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/InterationActions/LoadingInteractionAction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using TBydFramework.Runtime.Interactivity;
+using TBydFramework.Runtime.Views.UI;
+
+namespace TBydFramework.Runtime.Views.InterationActions
+{
+    public class LoadingInteractionAction : InteractionActionBase<VisibilityNotification>
+    {
+        private List<Loading> list = new List<Loading>();
+        public override void Action(VisibilityNotification notification, Action callback)
+        {
+            try
+            {
+                if (notification.Visible)
+                {
+                    Loading loading = Loading.Show(true);
+                    if (loading != null)
+                        list.Insert(0, loading);
+                }
+                else
+                {
+                    if (list.Count <= 0)
+                        return;
+
+                    Loading loading = list[0];
+                    list.RemoveAt(0);
+                    loading.Dispose();
+                }
+            }
+            finally
+            {
+                callback?.Invoke();
+            }
+        }
+    }
+}
