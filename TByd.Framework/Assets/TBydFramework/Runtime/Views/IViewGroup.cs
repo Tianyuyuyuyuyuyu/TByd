c96dfd2018bf04f61312a314d7fe58417ef631a0
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:27:27 +0800

    Create IViewGroup.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/IViewGroup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TBydFramework.Runtime.Views
+{
+    public delegate void Layout(Transform transform);
+
+    /// <summary>
+    /// View group
+    /// </summary>
+    public interface IViewGroup : IView
+    {
+        List<IView> Views { get; }
+
+        IView GetView(string name);
+
+        void AddView(IView view, bool worldPositionStays = false);
+
+        void AddView(IView view, Layout layout);
+
+        void RemoveView(IView view, bool worldPositionStays = false);
+    }
+}
