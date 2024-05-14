7a5e106e8404e584b166c0b258eea9fd5488929c
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:00:20 +0800

    Create LocalizedTextMesh.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Localizations/UI/LocalizedTextMesh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace TBydFramework.Runtime.Localizations.UI
+{
+    [AddComponentMenu("X/Localization/LocalizedTextMesh")]
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(TextMesh))]
+    public class LocalizedTextMesh : AbstractLocalized<TextMesh>
+    {
+        protected override void OnValueChanged(object sender, EventArgs e)
+        {
+            this.target.text = (string)Convert.ChangeType(this.value.Value, typeof(string));
+        }
+    }
+}
