e64a5964382f4b941937858786496222b91f40fa
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:28:56 +0800

    Create NotFoundException.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/NotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Views
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(Exception exception) : base("", exception)
+        {
+        }
+
+        public NotFoundException(string message, Exception exception) : base(message, exception)
+        {
+        }
+    }
+}
