9a71a2200f946f68f216b3985d09ebace3d5fcef
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:09:21 +0800

    Create ObservableObject.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Observables/ObservableObject.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using TBydFramework.Runtime.Log;
+
+namespace TBydFramework.Runtime.Observables
+{
+    [Serializable]
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(ObservableObject));
+
+        private static readonly PropertyChangedEventArgs NULL_EVENT_ARGS = new PropertyChangedEventArgs(null);
+        private static readonly Dictionary<string, PropertyChangedEventArgs> PROPERTY_EVENT_ARGS = new Dictionary<string, PropertyChangedEventArgs>();
+
+        private static PropertyChangedEventArgs GetPropertyChangedEventArgs(string propertyName)
+        {
+            if (propertyName == null)
+                return NULL_EVENT_ARGS;
+
+            PropertyChangedEventArgs eventArgs;
+            if (PROPERTY_EVENT_ARGS.TryGetValue(propertyName, out eventArgs))
+                return eventArgs;
+
+            eventArgs = new PropertyChangedEventArgs(propertyName);
+            PROPERTY_EVENT_ARGS[propertyName] = eventArgs;
+            return eventArgs;
+        }
+
+        private readonly object _lock = new object();
+        private PropertyChangedEventHandler propertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add { lock (_lock) { this.propertyChanged += value; } }
+            remove { lock (_lock) { this.propertyChanged -= value; } }
+        }
+
+        //[Conditional("DEBUG")]
+        //protected void VerifyPropertyName(string propertyName)
+        //{
+        //    var type = this.GetType();
+        //    if (!string.IsNullOrEmpty(propertyName) && type.GetProperty(propertyName) == null)
+        //        throw new ArgumentException("Property not found", propertyName);
+        //}
+
+        /// <summary>
+        /// Raises the PropertyChanging event.
+        /// </summary>
+        /// <param name="propertyName">Property name.</param>
+        protected virtual void RaisePropertyChanged(string propertyName = null)
+        {
+            //RaisePropertyChanged(new PropertyChangedEventArgs(propertyName));
+            RaisePropertyChanged(GetPropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Raises the PropertyChanging event.
+        /// </summary>
+        /// <param name="eventArgs">Property changed event.</param>
+        protected virtual void RaisePropertyChanged(PropertyChangedEventArgs eventArgs)
+        {
+            try
+            {
+                //VerifyPropertyName(eventArgs.PropertyName);
+
+                if (propertyChanged != null)
+                    propertyChanged(this, eventArgs);
+            }
+            catch (Exception e)
+            {
+                if (log.IsWarnEnabled)
+                    log.WarnFormat("Set property '{0}', raise PropertyChanged failure.Exception:{1}", eventArgs.PropertyName, e);
+            }
+        }
+
+        /// <summary>
+        /// Raises the PropertyChanging event.
+        /// </summary>
+        /// <param name="eventArgs"></param>
+        protected virtual void RaisePropertyChanged(params PropertyChangedEventArgs[] eventArgs)
+        {
+            foreach (var args in eventArgs)
+            {
+                try
+                {
+                    //VerifyPropertyName(args.PropertyName);
+
+                    if (propertyChanged != null)
+                        propertyChanged(this, args);
+                }
+                catch (Exception e)
+                {
+                    if (log.IsWarnEnabled)
+                        log.WarnFormat("Set property '{0}', raise PropertyChanged failure.Exception:{1}", args.PropertyName, e);
+                }
+            }
+        }
+
+        protected virtual string ParserPropertyName(LambdaExpression propertyExpression)
+        {
+            if (propertyExpression == null)
+                throw new ArgumentNullException("propertyExpression");
+
+            var body = propertyExpression.Body as MemberExpression;
+            if (body == null)
+                throw new ArgumentException("Invalid argument", "propertyExpression");
+
+            var property = body.Member as PropertyInfo;
+            if (property == null)
+                throw new ArgumentException("Argument is not a property", "propertyExpression");
+
+            return property.Name;
+        }
+
+        [Conditional("DEBUG")]
+        protected void VerifyPropertyType(Type type)
+        {
+            if (type.IsValueType)
+                log.Debug("Please use Set(field,newValue) instead of Set<T>(field,newValue) to avoid value types being boxed.");
+        }
+
+        /// <summary>
+        /// Set the specified propertyExpression, field and newValue.
+        /// </summary>
+        /// <param name="propertyExpression">Property expression.</param>
+        /// <param name="field">Field.</param>
+        /// <param name="newValue">New value.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        protected bool Set<T>(ref T field, T newValue, Expression<Func<T>> propertyExpression)
+        {
+            //VerifyPropertyType(typeof(T));
+            //if (object.Equals(field, newValue))
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+                return false;
+
+            field = newValue;
+            var propertyName = ParserPropertyName(propertyExpression);
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        ///  Set the specified propertyName, field, newValue.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="newValue"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected bool Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            //VerifyPropertyType(typeof(T));
+            //if (object.Equals(field, newValue))
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+                return false;
+
+            field = newValue;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        ///  Set the specified propertyName, field, newValue.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="newValue"></param>
+        /// <param name="eventArgs"></param>
+        /// <returns></returns>
+        protected bool Set<T>(ref T field, T newValue, PropertyChangedEventArgs eventArgs)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+                return false;
+
+            field = newValue;
+            RaisePropertyChanged(eventArgs);
+            return true;
+        }
+
+        /// <summary>
+        ///  Set the specified propertyName, field, newValue.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="newValue"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        [Obsolete]
+        protected bool SetValue<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null) where T : IEquatable<T>
+        {
+            if ((field != null && field.Equals(newValue)) || (field == null && newValue == null))
+                return false;
+
+            field = newValue;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        ///  Set the specified propertyName, field, newValue.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="newValue"></param>
+        /// <param name="eventArgs"></param>
+        /// <returns></returns>
+        [Obsolete]
+        protected bool SetValue<T>(ref T field, T newValue, PropertyChangedEventArgs eventArgs) where T : IEquatable<T>
+        {
+            if ((field != null && field.Equals(newValue)) || (field == null && newValue == null))
+                return false;
+
+            field = newValue;
+            RaisePropertyChanged(eventArgs);
+            return true;
+        }
+    }
+}
