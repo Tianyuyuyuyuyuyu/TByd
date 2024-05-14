34a276e8573862f80b93b38f9169917130e7f16f
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:40:54 +0800

    Create ExpressionPathFinder.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Paths/ExpressionPathFinder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace TBydFramework.Runtime.Binding.Paths
+{
+    public class ExpressionPathFinder : IExpressionPathFinder
+    {
+        //private static readonly ILog log = LogManager.GetLogger(typeof(ExpressionPathFinder));
+
+        public List<Path> FindPaths(LambdaExpression expression)
+        {
+            PathExpressionVisitor visitor = new PathExpressionVisitor();
+            visitor.Visit(expression);
+            return visitor.Paths;
+        }
+    }
+}
