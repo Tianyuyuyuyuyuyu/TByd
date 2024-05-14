b02a32d93fd97d7d2f0421479504ef4faaf2bf96
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:01:47 +0800

    Create VersionTypeConverter.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Localizations/VersionTypeConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Localizations
+{
+    public class VersionTypeConverter : ITypeConverter
+    {
+        public bool Support(string typeName)
+        {
+            switch (typeName)
+            {
+                case "version":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public Type GetType(string typeName)
+        {
+            switch (typeName)
+            {
+                case "version":
+                    return typeof(Version);
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        public object Convert(Type type, object value)
+        {
+            if (type == null)
+                throw new NotSupportedException();
+
+            string version = (string)value;
+            if (string.IsNullOrEmpty(version))
+                return new Version("0.0.0");
+            return new Version(version.Trim());
+        }
+    }
+}
