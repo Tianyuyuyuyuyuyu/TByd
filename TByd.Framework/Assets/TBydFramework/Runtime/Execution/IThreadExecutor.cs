2b614eccdb835ca858e382884473a8b5b4c5d3f3
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:55:19 +0800

    Create IThreadExecutor.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Execution/IThreadExecutor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TBydFramework.Runtime.Asynchronous;
+
+namespace TBydFramework.Runtime.Execution
+{
+    public interface IThreadExecutor
+    {
+        Asynchronous.IAsyncResult Execute(Action action);
+
+        IAsyncResult<TResult> Execute<TResult>(Func<TResult> func);
+
+        Asynchronous.IAsyncResult Execute(Action<Asynchronous.IPromise> action);
+
+        IProgressResult<TProgress> Execute<TProgress>(Action<IProgressPromise<TProgress>> action);
+
+        IAsyncResult<TResult> Execute<TResult>(Action<Asynchronous.IPromise<TResult>> action);
+
+        IProgressResult<TProgress, TResult> Execute<TProgress, TResult>(Action<IProgressPromise<TProgress, TResult>> action);
+    }
+}
