29d885d8cd8ad71bafcddd63820868fd5c210629
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:51:48 +0800

    Create TypeExtensions.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/TypeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace TBydFramework.Runtime.Binding
+{
+    public static class TypeExtensions
+    {
+        //private static readonly ILog log = LogManager.GetLogger(typeof(TypeExtensions));
+
+        public static MemberInfo FindFirstMemberInfo(this Type type, string name)
+        {
+            var members = type.GetMember(name);
+            if (members == null || members.Length <= 0)
+                return null;
+            return members[0];
+        }
+
+        public static MemberInfo FindFirstMemberInfo(this Type type, string name, BindingFlags flags)
+        {
+            var members = type.GetMember(name, flags);
+            if (members == null || members.Length <= 0)
+                return null;
+            return members[0];
+        }
+    }
+}
