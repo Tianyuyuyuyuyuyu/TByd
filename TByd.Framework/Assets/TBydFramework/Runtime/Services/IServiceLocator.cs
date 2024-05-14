86d6565faf1f507dabf1cf95736e4fbde0cf8548
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:16:19 +0800

    Create IServiceLocator.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Services/IServiceLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Services
+{
+    public interface IServiceLocator
+    {
+        object Resolve(Type type);
+
+        T Resolve<T>();
+
+        object Resolve(string name);
+
+        T Resolve<T>(string name);
+
+    }
+}
