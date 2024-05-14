a26a56fe876e39c5194b304ab29fe66609924b43
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:57:57 +0800

    Create LocalizedString.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Localizations/Data/LocalizedString.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TBydFramework.Runtime.Localizations.Data
+{
+    public class LocalizedString : LocalizedObject<string>
+    {
+        public LocalizedString() : base(null, Localization.Current)
+        {
+        }
+
+        public LocalizedString(IDictionary<string, string> source) : base(source, Localization.Current)
+        {
+        }
+
+        public LocalizedString(IDictionary<string, string> source, Localization localization) : base(source, localization)
+        {
+        }
+    }
+}
