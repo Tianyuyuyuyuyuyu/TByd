e8573f1616a8a9f8dcdc2772d7b0b574bffcfad6
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:14:26 +0800

    Create IDecryptor.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Security/Cryptography/IDecryptor.cs
@@ -0,0 +1,10 @@
+﻿namespace TBydFramework.Runtime.Security.Cryptography
+{
+    public interface IDecryptor
+    {
+        string AlgorithmName { get; }
+
+        byte[] Decrypt(byte[] buffer);
+
+    }
+}
