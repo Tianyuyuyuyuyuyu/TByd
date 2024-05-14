15ba38495886754b2918076f2ea64d795bf70335
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:53:17 +0800

    Create ITypeConverter.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Configuration/ITypeConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Configuration
+{
+    public interface ITypeConverter
+    {
+        bool Support(Type type);
+
+        object Convert(Type type, object value);
+    }
+}
