9e03c9c2dfff9f558f213867f2e80a69affb12f2
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:34:52 +0800

    Create StandardBinder.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Binders/StandardBinder.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using TBydFramework.Runtime.Binding.Contexts;
+
+namespace TBydFramework.Runtime.Binding.Binders
+{
+    public class StandardBinder : IBinder
+    {
+        protected IBindingFactory factory;
+
+        public StandardBinder(IBindingFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public IBinding Bind(IBindingContext bindingContext, object source, object target, BindingDescription bindingDescription)
+        {
+            return factory.Create(bindingContext, source, target, bindingDescription);
+        }
+
+        public IEnumerable<IBinding> Bind(IBindingContext bindingContext, object source, object target, IEnumerable<BindingDescription> bindingDescriptions)
+        {
+            if (bindingDescriptions == null)
+                return new IBinding[0];
+
+            return bindingDescriptions.Select(description => this.Bind(bindingContext, source, target, description));
+        }
+    }
+}
