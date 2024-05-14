396b30454bd63e6f2eb23f231ccb4f826e5e025f
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:55:37 +0800

    Create ThreadExecutor.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Execution/ThreadExecutor.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using TBydFramework.Runtime.Asynchronous;
+
+namespace TBydFramework.Runtime.Execution
+{
+    public class ThreadExecutor : AbstractExecutor, IThreadExecutor
+    {
+        public virtual Asynchronous.IAsyncResult Execute(Action action)
+        {
+            AsyncResult result = new AsyncResult(true);
+            Executors.RunAsyncNoReturn(() =>
+            {
+                try
+                {
+                    if (result.IsCancellationRequested)
+                    {
+                        result.SetCancelled();
+                        return;
+                    }
+
+                    action();
+                    result.SetResult();
+                }
+                catch (Exception e)
+                {
+                    result.SetException(e);
+                }
+            });
+            return result;
+        }
+
+        public virtual IAsyncResult<TResult> Execute<TResult>(Func<TResult> func)
+        {
+            AsyncResult<TResult> result = new AsyncResult<TResult>(true);
+            Executors.RunAsyncNoReturn(() =>
+            {
+                try
+                {
+                    if (result.IsCancellationRequested)
+                    {
+                        result.SetCancelled();
+                        return;
+                    }
+
+                    TResult tr = func();
+                    result.SetResult(tr);
+                }
+                catch (Exception e)
+                {
+                    result.SetException(e);
+                }
+            });
+            return result;
+        }
+
+        public virtual Asynchronous.IAsyncResult Execute(Action<Asynchronous.IPromise> action)
+        {
+            AsyncResult result = new AsyncResult(true);
+            Executors.RunAsyncNoReturn(() =>
+            {
+                try
+                {
+                    if (result.IsCancellationRequested)
+                    {
+                        result.SetCancelled();
+                        return;
+                    }
+
+                    action(result);
+                    if (!result.IsDone)
+                        result.SetResult(null);
+                }
+                catch (Exception e)
+                {
+                    if (!result.IsDone)
+                        result.SetException(e);
+                }
+            });
+            return result;
+        }
+
+        public virtual IProgressResult<TProgress> Execute<TProgress>(Action<IProgressPromise<TProgress>> action)
+        {
+            ProgressResult<TProgress> result = new ProgressResult<TProgress>(true);
+            Executors.RunAsyncNoReturn(() =>
+            {
+                try
+                {
+                    if (result.IsCancellationRequested)
+                    {
+                        result.SetCancelled();
+                        return;
+                    }
+
+                    action(result);
+                    if (!result.IsDone)
+                        result.SetResult(null);
+                }
+                catch (Exception e)
+                {
+                    if (!result.IsDone)
+                        result.SetException(e);
+                }
+            });
+            return result;
+        }
+
+        public virtual IAsyncResult<TResult> Execute<TResult>(Action<Asynchronous.IPromise<TResult>> action)
+        {
+            AsyncResult<TResult> result = new AsyncResult<TResult>(true);
+            Executors.RunAsyncNoReturn(() =>
+            {
+                try
+                {
+                    if (result.IsCancellationRequested)
+                    {
+                        result.SetCancelled();
+                        return;
+                    }
+
+                    action(result);
+                    if (!result.IsDone)
+                        result.SetResult(null);
+                }
+                catch (Exception e)
+                {
+                    if (!result.IsDone)
+                        result.SetException(e);
+                }
+            });
+            return result;
+        }
+
+        public virtual IProgressResult<TProgress, TResult> Execute<TProgress, TResult>(Action<IProgressPromise<TProgress, TResult>> action)
+        {
+            ProgressResult<TProgress, TResult> result = new ProgressResult<TProgress, TResult>(true);
+            Executors.RunAsyncNoReturn(() =>
+            {
+                try
+                {
+                    if (result.IsCancellationRequested)
+                    {
+                        result.SetCancelled();
+                        return;
+                    }
+
+                    action(result);
+                    if (!result.IsDone)
+                        result.SetResult(null);
+                }
+                catch (Exception e)
+                {
+                    if (!result.IsDone)
+                        result.SetException(e);
+                }
+            });
+            return result;
+        }
+    }
+}
