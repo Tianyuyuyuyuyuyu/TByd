428a068b50da67a2e04f46c43147574219810d83
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:43:14 +0800

    Create ISourceProxy.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Sources/ISourceProxy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Proxy.Sources
+{
+    public interface ISourceProxy : IBindingProxy
+    {
+        Type Type { get; }
+
+        TypeCode TypeCode { get; }
+
+        object Source { get; }
+    }
+}
