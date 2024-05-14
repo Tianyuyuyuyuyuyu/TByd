f5004312d4ea855687b1c7265900fb6c88316125
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 20:36:04 +0800

    Create ICrystalInfoRepository.cs

---
+++ TByd.Framework.Data/Assets/Samples/TByd Framework Data/0.0.1/Examples/Scripts/Repositories/ICrystalInfoRepository.cs
@@ -0,0 +1,11 @@
+﻿using TBydFramework.Examples.Domains;
+
+namespace TBydFramework.Examples.Repositories
+{
+    public interface ICrystalInfoRepository
+    {
+        CrystalInfo GetById(int id);
+
+        CrystalInfo GetBySign(string sign, int level);
+    }
+}
