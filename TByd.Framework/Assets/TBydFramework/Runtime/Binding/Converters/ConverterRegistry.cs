38b4c07383a2ac7023a494b675714e76788a566e
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:36:30 +0800

    Create ConverterRegistry.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Converters/ConverterRegistry.cs
@@ -0,0 +1,16 @@
+﻿using TBydFramework.Runtime.Binding.Registry;
+
+namespace TBydFramework.Runtime.Binding.Converters
+{
+    public class ConverterRegistry: KeyValueRegistry<string,IConverter>, IConverterRegistry
+    {
+        public ConverterRegistry()
+        {
+            this.Init();
+        }
+
+        protected virtual void Init()
+        {
+        }
+    }
+}
