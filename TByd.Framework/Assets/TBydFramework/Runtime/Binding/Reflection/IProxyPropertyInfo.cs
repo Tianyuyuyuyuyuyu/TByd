04164253a7a75d716a936e122a21821b39d35ff3
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:49:58 +0800

    Create IProxyPropertyInfo.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Reflection/IProxyPropertyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Reflection
+{
+    public interface IProxyPropertyInfo : IProxyMemberInfo
+    {
+        bool IsValueType { get; }
+
+        Type ValueType { get; }
+
+        TypeCode ValueTypeCode { get; }
+
+        object GetValue(object target);
+
+        void SetValue(object target, object value);
+    }
+
+    public interface IProxyPropertyInfo<TValue> : IProxyPropertyInfo
+    {
+        new TValue GetValue(object target);
+
+        void SetValue(object target, TValue value);
+    }
+
+    public interface IProxyPropertyInfo<T, TValue> : IProxyPropertyInfo<TValue>
+    {
+        TValue GetValue(T target);
+
+        void SetValue(T target, TValue value);
+    }
+}
