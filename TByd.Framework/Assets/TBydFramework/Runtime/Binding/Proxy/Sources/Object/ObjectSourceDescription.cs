72ae0b4ca43785a8d517fd5aa83469103c8c10b9
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:44:23 +0800

    Create ObjectSourceDescription.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Sources/Object/ObjectSourceDescription.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TBydFramework.Runtime.Binding.Paths;
+
+namespace TBydFramework.Runtime.Binding.Proxy.Sources.Object
+{
+    [Serializable]
+    public class ObjectSourceDescription : SourceDescription
+    {
+        private Path path;
+
+        public ObjectSourceDescription()
+        {
+            this.IsStatic = false;
+        }
+
+        public ObjectSourceDescription(Path path)
+        {
+            this.Path = path;
+        }
+
+        public virtual Path Path
+        {
+            get { return this.path; }
+            set
+            {
+                this.path = value;
+                if (this.path != null)
+                    this.IsStatic = this.path.IsStatic;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.path == null ? "Path:null" : "Path:" + this.path.ToString();
+        }
+    }
+}
