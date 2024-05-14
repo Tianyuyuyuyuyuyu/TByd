f5264d8c7c8a6b29bfd8ab9136391b2932397293
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:01:11 +0800

    Create LocalizationSourceAsset.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Localizations/Unity/LocalizationSourceAsset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace TBydFramework.Runtime.Localizations.Unity
+{
+    [CreateAssetMenu(fileName = "New Localization Module", menuName = "X/LocalizationSource", order = 1)]
+    public class LocalizationSourceAsset : ScriptableObject
+    {
+        public MonolingualSource Source = new MonolingualSource();
+    }
+}
