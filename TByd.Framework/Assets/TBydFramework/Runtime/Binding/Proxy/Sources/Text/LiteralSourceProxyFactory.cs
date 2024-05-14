5294a5e1e8be70bd1556594a16634f1542644bbb
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:45:25 +0800

    Create LiteralSourceProxyFactory.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Sources/Text/LiteralSourceProxyFactory.cs
@@ -0,0 +1,17 @@
+﻿using TBydFramework.Runtime.Observables;
+
+namespace TBydFramework.Runtime.Binding.Proxy.Sources.Text
+{
+    public class LiteralSourceProxyFactory : TypedSourceProxyFactory<LiteralSourceDescription>
+    {
+        protected override bool TryCreateProxy(object source, LiteralSourceDescription description, out ISourceProxy proxy)
+        {
+            var value = description.Literal;
+            if (value != null && value is IObservableProperty)
+                proxy = new ObservableLiteralSourceProxy(value as IObservableProperty);
+            else
+                proxy = new LiteralSourceProxy(value);
+            return true;
+        }
+    }
+}
