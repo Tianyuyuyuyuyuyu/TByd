01512a677a23774525414127ba75475200b25397
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:30:25 +0800

    Create LoadingWindow.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/UI/LoadingWindow.cs
@@ -0,0 +1,10 @@
+﻿namespace TBydFramework.Runtime.Views.UI
+{
+    public class LoadingWindow : Window
+    {
+        protected override void OnCreate(IBundle bundle)
+        {
+            this.WindowType = WindowType.PROGRESS;
+        }
+    }
+}
