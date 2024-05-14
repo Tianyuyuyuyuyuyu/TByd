ea39311e37a2f169966bdf461cec3b6242b9d25f
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:43:59 +0800

    Create INodeProxyFactoryRegister.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Sources/Object/INodeProxyFactoryRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace TBydFramework.Runtime.Binding.Proxy.Sources.Object
+{
+    public interface INodeProxyFactoryRegister
+    {
+        void Register(INodeProxyFactory factory,int priority = 100);
+
+        void Unregister(INodeProxyFactory factory);
+    }
+}
