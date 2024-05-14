928a96c45c5c542a7af8df9d7fb20e5199f10b32
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:34:02 +0800

    Create TaskYieldInstruction.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Asynchronous/TaskYieldInstruction.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TBydFramework.Runtime.Asynchronous
+{
+    public class TaskYieldInstruction : CustomYieldInstruction
+    {
+        private readonly Task task;
+        public TaskYieldInstruction(Task task)
+        {
+            this.task = task;
+        }
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                if (task.Exception != null)
+                    ExceptionDispatchInfo.Capture(task.Exception).Throw();
+
+                return !task.IsCompleted;
+            }
+        }
+    }
+
+    public class TaskYieldInstruction<T> : CustomYieldInstruction
+    {
+        private readonly Task<T> task;
+        public TaskYieldInstruction(Task<T> task)
+        {
+            this.task = task;
+        }
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                if (task.Exception != null)
+                    ExceptionDispatchInfo.Capture(task.Exception).Throw();
+
+                return !task.IsCompleted;
+            }
+        }
+    }
+}
