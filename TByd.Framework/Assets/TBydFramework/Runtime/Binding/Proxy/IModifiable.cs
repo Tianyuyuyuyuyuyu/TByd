890338770d2bebdd14074809a2f024ff965ae170
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:42:03 +0800

    Create IModifiable.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/IModifiable.cs
@@ -0,0 +1,14 @@
+﻿namespace TBydFramework.Runtime.Binding.Proxy
+{
+    public interface IModifiable
+    {
+        void SetValue(object value);
+
+        void SetValue<TValue>(TValue value);
+    }
+
+    public interface IModifiable<TValue> 
+    {
+        void SetValue(TValue value);
+    }
+}
