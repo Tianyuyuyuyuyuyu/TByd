5039cc0fc92c39bb5691c92503f33d0070120620
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:45:19 +0800

    Create LiteralSourceProxy.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Sources/Text/LiteralSourceProxy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Proxy.Sources.Text
+{
+    public class LiteralSourceProxy : SourceProxyBase, ISourceProxy, IObtainable
+    {
+        public LiteralSourceProxy(object source) : base(source)
+        {
+        }
+
+        public override Type Type { get { return this.source != null ? this.source.GetType() : typeof(object); } }
+
+        public virtual object GetValue()
+        {
+            return this.source;
+        }
+
+        public virtual TValue GetValue<TValue>()
+        {
+            return (TValue)Convert.ChangeType(this.source, typeof(TValue));
+        }
+    }
+}
