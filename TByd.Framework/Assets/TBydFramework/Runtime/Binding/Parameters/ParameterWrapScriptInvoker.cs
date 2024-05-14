c9cacd71ac50c4fd8c5b7a48e1533fde89aa63a5
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:40:45 +0800

    Create ParameterWrapScriptInvoker.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Parameters/ParameterWrapScriptInvoker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TBydFramework.Runtime.Binding.Proxy;
+
+namespace TBydFramework.Runtime.Binding.Parameters
+{
+    public class ParameterWrapScriptInvoker : ParameterWrapBase, IInvoker
+    {
+        private readonly IScriptInvoker invoker;
+
+        public ParameterWrapScriptInvoker(IScriptInvoker invoker, ICommandParameter commandParameter) : base(commandParameter)
+        {
+            if (invoker == null)
+                throw new ArgumentNullException("invoker");
+
+            this.invoker = invoker;
+        }
+
+        public object Invoke(params object[] args)
+        {
+            return this.invoker.Invoke(GetParameterValue());
+        }
+    }
+}
