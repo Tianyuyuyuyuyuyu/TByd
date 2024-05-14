37b63ef98af7904bb86e8cd26ddb33fb3a4ef349
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:46:37 +0800

    Create UnityFieldProxy.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Targets/UGUI/UnityFieldProxy.cs
@@ -0,0 +1,38 @@
+﻿using TBydFramework.Runtime.Binding.Proxy.Targets.Universal;
+using TBydFramework.Runtime.Binding.Reflection;
+using UnityEngine.Events;
+
+namespace TBydFramework.Runtime.Binding.Proxy.Targets.UGUI
+{
+    public class UnityFieldProxy<TValue> : FieldTargetProxy
+    {
+        //private static readonly ILog log = LogManager.GetLogger(typeof(UnityPropertyProxy<TValue>));
+
+        private UnityEvent<TValue> unityEvent;
+        public UnityFieldProxy(object target, IProxyFieldInfo fieldInfo, UnityEvent<TValue> unityEvent) : base(target, fieldInfo)
+        {
+            this.unityEvent = unityEvent;
+        }
+
+        public override BindingMode DefaultMode { get { return BindingMode.TwoWay; } }
+
+        protected override void DoSubscribeForValueChange(object target)
+        {
+            if (this.unityEvent == null || target == null)
+                return;
+
+            unityEvent.AddListener(OnValueChanged);
+        }
+
+        protected override void DoUnsubscribeForValueChange(object target)
+        {
+            if (unityEvent != null)
+                unityEvent.RemoveListener(OnValueChanged);
+        }
+
+        private void OnValueChanged(TValue value)
+        {
+            this.RaiseValueChanged();
+        }
+    }
+}
