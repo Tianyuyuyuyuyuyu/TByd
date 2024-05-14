116b0a067cef8de7d24dd4377b0aa9a66239cb22
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:07:39 +0800

    Create UnityGameObjectFactoryBase.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/ObjectPool/UnityGameObjectFactoryBase.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace TBydFramework.Runtime.ObjectPool
+{
+    public abstract class UnityGameObjectFactoryBase : IObjectFactory<GameObject>
+    {
+        public virtual GameObject Create(IObjectPool<GameObject> pool)
+        {
+            GameObject target = this.Create();
+            PooledUnityObject pooledObj = target.AddComponent<PooledUnityObject>();
+            pooledObj.pool = pool;
+            return target;
+        }
+
+        protected abstract GameObject Create();
+
+        public abstract void Reset(GameObject obj);
+
+        public virtual void Destroy(GameObject obj)
+        {
+            Object.Destroy(obj);
+        }
+
+        public virtual bool Validate(GameObject obj)
+        {
+            return true;
+        }
+
+        class PooledUnityObject : MonoBehaviour, IPooledObject
+        {
+            internal IObjectPool<GameObject> pool;
+
+            public void Free()
+            {
+                if (pool != null)
+                    pool.Free(this.gameObject);
+            }
+        }
+    }
+}
