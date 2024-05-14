0a94e6673c2569330c03d6327c3f67884e989cdc
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:42:21 +0800

    Create IScriptInvoker.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/IScriptInvoker.cs
@@ -0,0 +1,9 @@
+﻿namespace TBydFramework.Runtime.Binding.Proxy
+{
+    /// <summary>
+    /// Supports Lua Function.
+    /// </summary>
+    public interface IScriptInvoker: IInvoker
+    {
+    }
+}
