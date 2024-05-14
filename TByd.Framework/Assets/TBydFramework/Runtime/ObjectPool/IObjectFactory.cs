58fcd507bafe9677538663185dcb79f7de98ec6e
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:06:19 +0800

    Create IObjectFactory.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/ObjectPool/IObjectFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace TBydFramework.Runtime.ObjectPool
+{
+    public interface IObjectFactory<T> where T : class
+    {
+        /// <summary>
+        /// Create a <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="pool"></param>
+        /// <returns></returns>
+        T Create(IObjectPool<T> pool);
+
+        /// <summary>
+        /// Destroy the object.
+        /// </summary>
+        /// <param name="obj"></param>
+        void Destroy(T obj);
+
+        /// <summary>
+        /// Reset the object
+        /// </summary>
+        /// <param name="obj"></param>
+        void Reset(T obj);
+
+        /// <summary>
+        /// Validate the object
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        bool Validate(T obj);
+    }
+}
