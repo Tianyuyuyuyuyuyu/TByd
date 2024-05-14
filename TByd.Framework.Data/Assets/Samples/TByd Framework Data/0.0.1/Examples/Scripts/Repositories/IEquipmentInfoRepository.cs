1a9b1af44975a30038a47e514eafa7aded3ce8ba
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 20:36:13 +0800

    Create IEquipmentInfoRepository.cs

---
+++ TByd.Framework.Data/Assets/Samples/TByd Framework Data/0.0.1/Examples/Scripts/Repositories/IEquipmentInfoRepository.cs
@@ -0,0 +1,11 @@
+﻿using TBydFramework.Examples.Domains;
+
+namespace TBydFramework.Examples.Repositories
+{
+    public interface IEquipmentInfoRepository
+    {
+        EquipmentInfo GetById(int id);
+
+        EquipmentInfo GetBySign(string sign, int quality);
+    }
+}
