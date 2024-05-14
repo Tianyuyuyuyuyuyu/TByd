6c1d7a0f88f4e7eb1157eb94c76a75b3c3f31944
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:23:59 +0800

    Create GlobalSetting.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/GlobalSetting.cs
@@ -0,0 +1,15 @@
+﻿namespace TBydFramework.Runtime.Views
+{
+    public class GlobalSetting
+    {
+        /// <summary>
+        /// Whether to use the CanvasGroup.blocksRaycasts instead of the CanvasGroup.interactable to control the interactivity of the view  
+        /// </summary>
+        public static bool useBlocksRaycastsInsteadOfInteractable = false;
+
+        /// <summary>
+        /// Whether to enable the window state broadcast feature.
+        /// </summary>
+        public static bool enableWindowStateBroadcast = true;
+    }
+}
