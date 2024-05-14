1d675bd65df71d4e11df60df4d2979ed4f9daab9
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:50:10 +0800

    Create IProxyType.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Reflection/IProxyType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace TBydFramework.Runtime.Binding.Reflection
+{
+    public interface IProxyType
+    {
+        Type Type { get; }
+
+        IProxyMemberInfo GetMember(string name);
+
+        IProxyMemberInfo GetMember(string name, BindingFlags flags);
+
+        IProxyFieldInfo GetField(string name);
+
+        IProxyFieldInfo GetField(string name, BindingFlags flags);
+
+        IProxyPropertyInfo GetProperty(string name);
+
+        IProxyPropertyInfo GetProperty(string name, BindingFlags flags);
+
+        IProxyItemInfo GetItem();
+
+        IProxyMethodInfo GetMethod(string name);
+
+        IProxyMethodInfo GetMethod(string name, BindingFlags flags);
+
+        IProxyMethodInfo GetMethod(string name, Type[] parameterTypes);
+
+        IProxyMethodInfo GetMethod(string name, Type[] parameterTypes, BindingFlags flags);
+
+        IProxyEventInfo GetEvent(string name);
+    }
+}
