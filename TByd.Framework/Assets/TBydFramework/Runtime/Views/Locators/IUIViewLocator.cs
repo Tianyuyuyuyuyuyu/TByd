745c2e99b9e8cffa6c45bf5bec9784cd62cb442c
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:28:33 +0800

    Create IUIViewLocator.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/Locators/IUIViewLocator.cs
@@ -0,0 +1,31 @@
+﻿using TBydFramework.Runtime.Asynchronous;
+
+namespace TBydFramework.Runtime.Views.Locators
+{
+    public interface IUIViewLocator
+    {
+        IView LoadView(string name);
+
+        T LoadView<T>(string name) where T : IView;
+
+        IWindow LoadWindow(string name);
+
+        T LoadWindow<T>(string name) where T : IWindow;
+
+        IWindow LoadWindow(IWindowManager windowManager, string name);
+
+        T LoadWindow<T>(IWindowManager windowManager, string name) where T : IWindow;
+
+        IProgressResult<float, IView> LoadViewAsync(string name);
+
+        IProgressResult<float, T> LoadViewAsync<T>(string name) where T : IView;
+
+        IProgressResult<float, IWindow> LoadWindowAsync(string name);
+
+        IProgressResult<float, T> LoadWindowAsync<T>(string name) where T : IWindow;
+
+        IProgressResult<float, IWindow> LoadWindowAsync(IWindowManager windowManager, string name);
+
+        IProgressResult<float, T> LoadWindowAsync<T>(IWindowManager windowManager, string name) where T : IWindow;
+    }
+}
