7e1fd4338eb78e3499cd0bd48c9d5b906f10f2df
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:45:13 +0800

    Create LiteralSourceDescription.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Sources/Text/LiteralSourceDescription.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace TBydFramework.Runtime.Binding.Proxy.Sources.Text
+{
+    [Serializable]
+    public class LiteralSourceDescription : SourceDescription
+    {
+        public object Literal { get; set; }
+
+        public LiteralSourceDescription()
+        {
+            this.IsStatic = true;
+        }
+
+        public override string ToString()
+        {
+            return this.Literal == null ? "Literal:null" : "Literal:" + this.Literal.ToString();
+        }
+    }
+}
