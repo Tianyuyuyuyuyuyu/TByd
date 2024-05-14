403bdb50b139cc29ebb29919e9273509e416051f
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:34:22 +0800

    Create RemarkAttribute.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Attributes/RemarkAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Enum)]
+    public class RemarkAttribute : Attribute
+    {
+        private string remark;
+        public RemarkAttribute(string remark)
+        {
+            this.remark = remark;
+        }
+
+        public string Remark { get { return this.remark; } }
+    }
+}
