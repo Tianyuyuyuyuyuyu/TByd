c2c49e8e908eb9e84705b2c50c09608f3aac4b04
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:58:12 +0800

    Create IDataProvider.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Localizations/IDataProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace TBydFramework.Runtime.Localizations
+{
+    public interface IDataProvider
+    {
+        Task<Dictionary<string, object>> Load(CultureInfo cultureInfo);
+    }
+}
