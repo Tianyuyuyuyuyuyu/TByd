72ae4e24bd93db380d182280fdc7933abb384d62
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 19:51:19 +0800

    Create IHandshakeHandler.cs

---
+++ TByd.Framework.Connection/Assets/TBydFramework/Connection/Runtime/IHandshakeHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace TBydFramework.Connection.Runtime
+{
+    public interface IHandshakeHandler
+    {
+        /// <summary>
+        /// Send and receive handshake messages, 
+        /// return after completing the handshake.
+        /// </summary>
+        /// <param name="channel"></param>
+        /// <returns></returns>
+        Task OnHandshake(IChannel<IMessage> channel);
+    }
+}
