0cf9e87587c9c53d9554299659afac2ff83efb28
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:52:27 +0800

    Create ICommand.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Commands/ICommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Commands
+{
+    /// <summary>
+    /// 
+    /// </summary>
+	public interface ICommand
+	{
+		/// <summary>
+		/// Occurs when can execute changed.
+		/// </summary>
+		event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Determines whether this instance can execute the specified parameter.
+        /// </summary>
+        /// <param name="parameter">Parameter.</param>
+        /// <returns><c>true</c> if this instance can execute the specified parameter; otherwise, <c>false</c>.</returns>
+        bool CanExecute (object parameter);
+
+		/// <summary>
+		/// Execute the specified parameter.
+		/// </summary>
+		/// <param name="parameter">Parameter.</param>
+		void Execute (object parameter);
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public interface ICommand<T> : ICommand
+	{
+		/// <summary>
+		/// Determines whether this instance can execute the specified parameter.
+		/// </summary>
+		/// <param name="parameter">Parameter.</param>
+		/// <returns><c>true</c> if this instance can execute the specified parameter; otherwise, <c>false</c>.</returns>
+		bool CanExecute(T parameter);
+
+		/// <summary>
+		/// Execute the specified parameter.
+		/// </summary>
+		/// <param name="parameter">Parameter.</param>
+		void Execute(T parameter);
+	}
+}
+
