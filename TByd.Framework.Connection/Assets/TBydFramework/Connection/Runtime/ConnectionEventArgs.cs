576d0ba1baa96a7287fb6011e71e6d1f941069fb
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 19:50:08 +0800

    Create ConnectionEventArgs.cs

---
+++ TByd.Framework.Connection/Assets/TBydFramework/Connection/Runtime/ConnectionEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TBydFramework.Connection.Runtime
+{
+    public class ConnectionEventArgs : EventArgs
+    {
+        public static readonly ConnectionEventArgs ConnectingEventArgs = new ConnectionEventArgs("Connecting");
+        public static readonly ConnectionEventArgs ReconnectingEventArgs = new ConnectionEventArgs("Reconnecting");
+        public static readonly ConnectionEventArgs ConnectedEventArgs = new ConnectionEventArgs("Connected");
+        public static readonly ConnectionEventArgs FailedEventArgs = new ConnectionEventArgs("Failed");
+        public static readonly ConnectionEventArgs ExceptionEventArgs = new ConnectionEventArgs("Exception");
+        public static readonly ConnectionEventArgs ClosingEventArgs = new ConnectionEventArgs("Closing");
+        public static readonly ConnectionEventArgs ClosedEventArgs = new ConnectionEventArgs("Closed");
+
+        public ConnectionEventArgs(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; }
+
+        public override string ToString()
+        {
+            return string.Format("ConnectionEvent:[{0}]", this.Name);
+        }
+    }
+}
