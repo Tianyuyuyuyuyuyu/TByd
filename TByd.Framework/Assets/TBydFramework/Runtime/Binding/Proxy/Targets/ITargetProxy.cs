831cac3922cb209f9091b1eb81d5347ad67470a3
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:45:46 +0800

    Create ITargetProxy.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Targets/ITargetProxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Proxy.Targets
+{
+    public interface ITargetProxy : IBindingProxy
+    {
+        Type Type { get; }
+
+        TypeCode TypeCode { get; }
+
+        object Target { get; }
+
+        BindingMode DefaultMode { get; }
+    }
+}
