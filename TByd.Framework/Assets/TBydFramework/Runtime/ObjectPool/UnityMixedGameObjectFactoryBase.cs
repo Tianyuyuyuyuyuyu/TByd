032fadf6a01c30e9717ead41708101c8b09e99cb
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:08:03 +0800

    Create UnityMixedGameObjectFactoryBase.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/ObjectPool/UnityMixedGameObjectFactoryBase.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace TBydFramework.Runtime.ObjectPool
+{
+    public abstract class UnityMixedGameObjectFactoryBase : IMixedObjectFactory<GameObject>
+    {
+        public virtual GameObject Create(IMixedObjectPool<GameObject> pool, string typeName)
+        {
+            GameObject target = this.Create(typeName);
+            PooledUnityObject pooledObj = target.gameObject.AddComponent<PooledUnityObject>();
+            pooledObj.pool = pool;
+            pooledObj.target = target;
+            pooledObj.typeName = typeName;
+            return target;
+        }
+
+        protected abstract GameObject Create(string typeName);
+
+        public abstract void Reset(string typeName, GameObject obj);
+
+        public virtual void Destroy(string typeName, GameObject obj)
+        {
+            Object.Destroy(obj);
+        }
+
+        public virtual bool Validate(string typeName, GameObject obj)
+        {
+            return true;
+        }
+
+        class PooledUnityObject : MonoBehaviour, IPooledObject
+        {
+            internal IMixedObjectPool<GameObject> pool;
+            internal GameObject target;
+            internal string typeName;
+
+            public void Free()
+            {
+                if (pool != null)
+                    pool.Free(typeName, target);
+            }
+        }
+    }
+}
