54cd74267f420dd283ef21efd7b8d599465fd460
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:31:13 +0800

    Create ToastViewBase.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/UI/ToastViewBase.cs
@@ -0,0 +1,21 @@
+﻿namespace TBydFramework.Runtime.Views.UI
+{
+    public abstract class ToastViewBase : UIView
+    {
+        protected string content;
+        public string Content 
+        {
+            get { return this.content; }
+            set 
+            {
+                if (string.Equals(content,value))
+                    return;
+
+                this.content = value;
+                this.OnContentChanged();
+            }
+        }
+
+        protected abstract void OnContentChanged();
+    }
+}
