4e438786a4f3f7e68905db3a7bf3196cfc567868
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 19:49:50 +0800

    Create IMessageDecoder.cs

---
+++ TByd.Framework.Connection/Assets/TBydFramework/Connection/Runtime/Codec/IMessageDecoder.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using TBydFramework.Connection.Runtime.IO;
+
+namespace TBydFramework.Connection.Runtime.Codec
+{
+    public interface IMessageDecoder<TMessage>
+    {
+        Task<TMessage> Decode(BinaryReader reader);
+
+    }
+}
