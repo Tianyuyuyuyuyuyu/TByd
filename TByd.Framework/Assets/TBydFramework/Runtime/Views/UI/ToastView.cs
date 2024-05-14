ad167656ec489fc37363643a5278554da01adbb5
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:31:01 +0800

    Create ToastView.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/UI/ToastView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.UI;
+
+namespace TBydFramework.Runtime.Views.UI
+{
+    public class ToastView : ToastViewBase
+    {
+        public Text text;
+
+        protected override void OnContentChanged()
+        {
+            if (this.text != null)
+                this.text.text = this.content;
+        }
+    }
+}
