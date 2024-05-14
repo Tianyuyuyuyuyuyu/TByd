32e278cbd63312d38347b88f9975ee757fae7859
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:39:57 +0800

    Create ExpressionCommandParameter.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Parameters/ExpressionCommandParameter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Parameters
+{
+    public class ExpressionCommandParameter<TParam> : ICommandParameter<TParam>
+    {
+        private Func<TParam> expression;
+        public ExpressionCommandParameter(Func<TParam> expression)
+        {
+            this.expression = expression;
+        }
+
+        object ICommandParameter.GetValue()
+        {
+            return GetValue();
+        }
+
+        public Type GetValueType()
+        {
+            return typeof(TParam);
+        }
+
+        public TParam GetValue()
+        {
+            return expression();
+        }
+    }
+}
