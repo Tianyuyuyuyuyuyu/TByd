df0348da256ff8d68685ee2c960bf79c4c9be394
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:43:26 +0800

    Create ISourceProxyFactoryRegistry.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Sources/ISourceProxyFactoryRegistry.cs
@@ -0,0 +1,9 @@
+﻿namespace TBydFramework.Runtime.Binding.Proxy.Sources
+{
+    public interface ISourceProxyFactoryRegistry
+    {
+        void Register(ISourceProxyFactory factory, int priority = 100);
+
+        void Unregister(ISourceProxyFactory factory);
+    }
+}
