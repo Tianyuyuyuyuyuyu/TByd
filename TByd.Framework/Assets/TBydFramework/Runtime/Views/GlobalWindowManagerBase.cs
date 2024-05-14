8a4e78d152d89edeaf411233ab7dc66e370cb2e7
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:24:22 +0800

    Create GlobalWindowManagerBase.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/GlobalWindowManagerBase.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace TBydFramework.Runtime.Views
+{
+    [DisallowMultipleComponent]
+    public abstract class GlobalWindowManagerBase : WindowManager
+    {
+        public static GlobalWindowManagerBase Root;
+
+        protected virtual void Start()
+        {
+            Root = this;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            Root = null;
+        }
+    }
+}
