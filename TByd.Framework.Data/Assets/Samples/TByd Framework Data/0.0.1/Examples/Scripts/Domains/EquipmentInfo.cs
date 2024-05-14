4edc139c99d1dd8878184fd5dc1d842a925a5469
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 20:35:42 +0800

    Create EquipmentInfo.cs

---
+++ TByd.Framework.Data/Assets/Samples/TByd Framework Data/0.0.1/Examples/Scripts/Domains/EquipmentInfo.cs
@@ -0,0 +1,60 @@
+﻿using TBydFramework.Runtime.Localizations.Data;
+#if NEWTONSOFT
+using Newtonsoft.Json;
+#endif
+
+namespace TBydFramework.Examples.Domains
+{
+    public class EquipmentInfo
+    {
+#if NEWTONSOFT
+        [JsonProperty("id")]
+#endif
+        public int Id { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("sign")]
+#endif
+        public string Sign { get; protected set; }
+
+#if NEWTONSOFT
+        [JsonProperty("name")]
+#endif
+        public LocalizedString Name { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("category")]
+#endif
+        public Category Category { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("quality")]
+#endif
+        public int Quality { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("health")]
+#endif
+        public float Health { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("attackDamage")]
+#endif
+        public float AttackDamage { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("abilityPower")]
+#endif
+        public float AbilityPower { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("armor")]
+#endif
+        public float Armor { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("magicResist")]
+#endif
+        public float MagicResist { get; set; }
+    }
+}
