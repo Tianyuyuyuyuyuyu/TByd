dd7abdd9563b0397a235dc6faafad1a6a21fca5d
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:08:33 +0800

    Create IObservableProperty.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Observables/IObservableProperty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Observables
+{
+    public interface IObservableProperty
+    {
+        event EventHandler ValueChanged;
+
+        Type Type { get; }
+
+        object Value { get; set; }
+    }
+
+    public interface IObservableProperty<T> : IObservableProperty
+    {
+        new T Value { get; set; }
+    }
+}
