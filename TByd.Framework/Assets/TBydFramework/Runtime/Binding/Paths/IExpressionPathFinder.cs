9fc293ad54f871b8aef188d1835ca8565e5edfa3
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:41:00 +0800

    Create IExpressionPathFinder.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Paths/IExpressionPathFinder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace TBydFramework.Runtime.Binding.Paths
+{
+    public interface IExpressionPathFinder
+    {
+        List<Path> FindPaths(LambdaExpression expression);
+        
+    }
+}
