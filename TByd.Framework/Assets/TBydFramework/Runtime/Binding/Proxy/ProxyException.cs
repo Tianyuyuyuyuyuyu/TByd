4bd1427fda7b235676e2f3a9e422369adf304ad8
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:42:26 +0800

    Create ProxyException.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/ProxyException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Proxy
+{
+    public class ProxyException : BindingException
+    {
+        public ProxyException()
+        {
+        }
+
+        public ProxyException(string message) : base(message)
+        {
+        }
+
+        public ProxyException(string message, Exception exception) : base(message, exception)
+        {
+        }
+
+        public ProxyException(string format, params object[] arguments) : base(string.Format(format, arguments))
+        {
+        }
+
+        public ProxyException(Exception exception, string format, params object[] arguments) : base(string.Format(format, arguments), exception)
+        {
+        }
+    }
+}
