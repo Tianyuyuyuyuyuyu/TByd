1f98019c3df1b7715e6670ce77a0c6902a41a277
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:55:02 +0800

    Create IMainLoopExecutor.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Execution/IMainLoopExecutor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Execution
+{
+    public interface IMainLoopExecutor
+    {
+        void RunOnMainThread(Action action, bool waitForExecution = false);
+
+        TResult RunOnMainThread<TResult>(Func<TResult> func);
+    }
+}
