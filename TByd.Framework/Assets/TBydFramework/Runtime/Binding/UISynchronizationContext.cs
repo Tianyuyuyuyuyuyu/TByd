cca388b616bead0d07c8ee6d06313f30fbba6a7d
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:51:54 +0800

    Create UISynchronizationContext.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/UISynchronizationContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using UnityEngine;
+
+namespace TBydFramework.Runtime.Binding
+{
+    public class UISynchronizationContext
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void OnInitialize()
+        {
+            context = SynchronizationContext.Current;
+            threadId = Thread.CurrentThread.ManagedThreadId;
+        }
+
+        private static int threadId;
+        private static SynchronizationContext context;
+
+        public static bool InThread { get { return threadId == Thread.CurrentThread.ManagedThreadId; } }
+
+        public static void Post(SendOrPostCallback callback, object state)
+        {
+            if (threadId == Thread.CurrentThread.ManagedThreadId)
+                callback(state);
+            else
+                context.Post(callback, state);
+        }
+        public static void Send(SendOrPostCallback callback, object state)
+        {
+            if (threadId == Thread.CurrentThread.ManagedThreadId)
+                callback(state);
+            else
+                context.Send(callback, state);
+        }
+    }
+}
