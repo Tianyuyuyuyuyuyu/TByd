f772120fa98e0472e3644c398c94221979ad8033
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 19:52:48 +0800

    Create ISubscription.cs

---
+++ TByd.Framework.Connection/Assets/TBydFramework/Connection/Runtime/Subscription/ISubscription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace TBydFramework.Connection.Runtime.Subscription
+{
+    public interface ISubscription<T> : IDisposable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        ISubscription<T> Filter(Predicate<T> filter);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        ISubscription<T> ObserveOn(SynchronizationContext context);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        ISubscription<T> Subscribe(Action<T> action);
+    }
+}
