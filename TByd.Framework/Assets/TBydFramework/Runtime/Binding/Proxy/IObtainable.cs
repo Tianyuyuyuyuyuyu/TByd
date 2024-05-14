aa6cbd654efeb6cd44efd799370d420fbbf130bf
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:42:15 +0800

    Create IObtainable.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/IObtainable.cs
@@ -0,0 +1,14 @@
+﻿namespace TBydFramework.Runtime.Binding.Proxy
+{
+    public interface IObtainable
+    {
+        object GetValue();
+
+        TValue GetValue<TValue>();
+    }
+
+    public interface IObtainable<TValue> 
+    {
+        TValue GetValue();
+    }
+}
