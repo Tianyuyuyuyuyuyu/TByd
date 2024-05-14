18ecf3dea3b039cffac15f46cdd66da8d1bdbf54
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:59:29 +0800

    Create LocalizedAudioSourceInResources.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Localizations/UI/LocalizedAudioSourceInResources.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using TBydFramework.Runtime.Log;
+using UnityEngine;
+
+namespace TBydFramework.Runtime.Localizations.UI
+{
+    [AddComponentMenu("X/Localization/LocalizedAudioSourceInResources")]
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(AudioSource))]
+    public class LocalizedAudioSourceInResources : AbstractLocalized<AudioSource>
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(LocalizedAudioSourceInResources));
+
+        protected override void OnValueChanged(object sender, EventArgs e)
+        {
+            object v = this.value.Value;
+            if (v is AudioClip)
+            {
+                this.target.clip = (AudioClip)v;
+            }
+            else if (v is string)
+            {
+                string path = (string)v;
+                this.StartCoroutine(DoLoad(path));
+            }
+            else if (v != null)
+            {
+                if (log.IsErrorEnabled)
+                    log.ErrorFormat("There is an invalid localization value \"{0}\" on the GameObject named \"{1}\".", v, this.name);
+            }
+        }
+
+        protected virtual IEnumerator DoLoad(string path)
+        {
+            var result = Resources.LoadAsync<AudioClip>(path);
+            yield return result;
+            this.target.clip = (AudioClip)result.asset;
+        }
+    }
+}
