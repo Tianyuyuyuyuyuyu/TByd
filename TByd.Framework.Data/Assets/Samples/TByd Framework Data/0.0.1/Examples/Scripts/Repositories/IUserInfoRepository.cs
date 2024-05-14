1cfd48a0f4ac7afaec2c464cae80e912909616c9
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 20:36:22 +0800

    Create IUserInfoRepository.cs

---
+++ TByd.Framework.Data/Assets/Samples/TByd Framework Data/0.0.1/Examples/Scripts/Repositories/IUserInfoRepository.cs
@@ -0,0 +1,11 @@
+﻿using TBydFramework.Examples.Domains;
+
+namespace TBydFramework.Examples.Repositories
+{
+    public interface IUserInfoRepository
+    {
+        UserInfo GetById(int id);
+
+        UserInfo GetByUsername(string username);
+    }
+}
