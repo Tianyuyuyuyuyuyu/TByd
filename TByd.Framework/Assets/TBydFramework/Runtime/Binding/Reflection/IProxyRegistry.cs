e2ec0413c1fefc51aaa0cc30d757e018bca17bff
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:50:04 +0800

    Create IProxyRegistry.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Reflection/IProxyRegistry.cs
@@ -0,0 +1,11 @@
+﻿namespace TBydFramework.Runtime.Binding.Reflection
+{
+    public interface IProxyRegistry
+    {
+        void Register(IProxyFieldInfo info);
+
+        void Register(IProxyPropertyInfo info);
+
+        void Register(IProxyMethodInfo info);
+    }
+}
