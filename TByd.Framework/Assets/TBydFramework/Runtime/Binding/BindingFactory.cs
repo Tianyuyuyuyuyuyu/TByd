c01e2b5bd9ceeb60a737326ca0080a48f4c39470
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:35:16 +0800

    Create BindingFactory.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/BindingFactory.cs
@@ -0,0 +1,35 @@
+﻿using TBydFramework.Runtime.Binding.Contexts;
+using TBydFramework.Runtime.Binding.Proxy.Sources;
+using TBydFramework.Runtime.Binding.Proxy.Targets;
+
+namespace TBydFramework.Runtime.Binding
+{
+
+    public class BindingFactory : IBindingFactory
+    {
+        private ISourceProxyFactory sourceProxyFactory;
+        private ITargetProxyFactory targetProxyFactory;
+
+        public ISourceProxyFactory SourceProxyFactory
+        {
+            get { return this.sourceProxyFactory; }
+            set { this.sourceProxyFactory = value; }
+        }
+        public ITargetProxyFactory TargetProxyFactory
+        {
+            get { return this.targetProxyFactory; }
+            set { this.targetProxyFactory = value; }
+        }
+
+        public BindingFactory(ISourceProxyFactory sourceProxyFactory, ITargetProxyFactory targetProxyFactory)
+        {
+            this.sourceProxyFactory = sourceProxyFactory;
+            this.targetProxyFactory = targetProxyFactory;
+        }
+
+        public IBinding Create(IBindingContext bindingContext, object source, object target, BindingDescription bindingDescription)
+        {
+            return new Binding(bindingContext, source, target, bindingDescription, this.sourceProxyFactory, this.targetProxyFactory);
+        }
+    }
+}
