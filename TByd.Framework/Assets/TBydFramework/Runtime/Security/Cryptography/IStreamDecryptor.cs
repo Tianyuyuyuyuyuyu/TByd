2849f370dc3146b542d817fb2d96309d7097e592
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:14:49 +0800

    Create IStreamDecryptor.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Security/Cryptography/IStreamDecryptor.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace TBydFramework.Runtime.Security.Cryptography
+{
+    public interface IStreamDecryptor : IDecryptor
+    {
+        Stream Decrypt(Stream input);
+
+    }
+}
