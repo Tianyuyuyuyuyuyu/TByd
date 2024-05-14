12b51f64902a3367e70ccf7577a812962a28b874
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 20:35:51 +0800

    Create UserInfo.cs

---
+++ TByd.Framework.Data/Assets/Samples/TByd Framework Data/0.0.1/Examples/Scripts/Domains/UserInfo.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using TBydFramework.Runtime.Localizations.Data;
+#if NEWTONSOFT
+using Newtonsoft.Json;
+#endif
+
+namespace TBydFramework.Examples.Domains
+{
+    public class UserInfo
+    {
+#if NEWTONSOFT
+        [JsonProperty("id")]
+#endif
+        public int Id { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("username")]
+#endif
+        public string Username { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("name")]
+#endif
+        public LocalizedString Name { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("emails")]
+#endif
+        public List<string> Emails { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("information")]
+#endif
+        public LocalizedString Information { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("address")]
+#endif
+        public Address Address { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("status")]
+#endif
+        public Status Status { get; set; }
+    }
+
+    public class Address
+    {
+#if NEWTONSOFT
+        [JsonProperty("province")]
+#endif
+        public string Province { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("street")]
+#endif
+        public string Street { get; set; }
+
+#if NEWTONSOFT
+        [JsonProperty("postcode")]
+#endif
+        public string Postcode { get; set; }
+    }
+
+    public enum Status
+    {
+        OK,
+        LOCKED,
+        DELETED
+    }
+}
