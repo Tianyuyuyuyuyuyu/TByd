558fb3578908884c9350619b6c4c50c043567ecd
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 20:36:58 +0800

    Create LiteDBCrystalInfoRepository.cs

---
+++ TByd.Framework.Data/Assets/Samples/TByd Framework Data/0.0.1/Examples/Scripts/Repositories/LiteDBCrystalInfoRepository.cs
@@ -0,0 +1,24 @@
+﻿#if LITEDB
+using LiteDB;
+using TBydFramework.Examples.Domains;
+
+namespace TBydFramework.Examples.Repositories
+{
+    public class LiteDBCrystalInfoRepository : LiteDBRepository<CrystalInfo>, ICrystalInfoRepository
+    {
+        public LiteDBCrystalInfoRepository(ILiteDatabase database) : base(database)
+        {
+        }
+
+        public CrystalInfo GetById(int id)
+        {
+            return GetCollection().FindById(id);
+        }
+
+        public CrystalInfo GetBySign(string sign, int level)
+        {
+            return GetCollection().FindOne(c => c.Sign.Equals(sign) && c.Level == level);
+        }
+    }
+}
+#endif
