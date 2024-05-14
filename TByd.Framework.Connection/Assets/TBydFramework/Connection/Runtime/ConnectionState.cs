611468758459cc08648a92ad28bd50c9dffce626
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 19:50:17 +0800

    Create ConnectionState.cs

---
+++ TByd.Framework.Connection/Assets/TBydFramework/Connection/Runtime/ConnectionState.cs
@@ -0,0 +1,13 @@
+﻿namespace TBydFramework.Connection.Runtime
+{
+    /// <summary>
+    /// </summary>
+    public enum ConnectionState
+    {
+        Connecting,
+        Connected,
+        Exception,
+        Closing,
+        Closed
+    }
+}
