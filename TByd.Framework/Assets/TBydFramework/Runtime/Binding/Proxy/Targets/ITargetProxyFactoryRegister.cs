1c1ada374aeec4b840a3ead777542fa3bcf50cd4
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:45:58 +0800

    Create ITargetProxyFactoryRegister.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Targets/ITargetProxyFactoryRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace TBydFramework.Runtime.Binding.Proxy.Targets
+{
+    public interface ITargetProxyFactoryRegister
+    {
+        void Register(ITargetProxyFactory factory, int priority = 100);
+
+        void Unregister(ITargetProxyFactory factory);
+    }
+}
