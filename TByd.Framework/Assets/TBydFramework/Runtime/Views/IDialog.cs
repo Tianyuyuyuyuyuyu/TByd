19fe3f401f58570e5145c61014a1fd2d8c1974d9
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:24:46 +0800

    Create IDialog.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/IDialog.cs
@@ -0,0 +1,30 @@
+﻿namespace TBydFramework.Runtime.Views
+{
+    public interface IDialog
+    {
+        /// <summary>
+        /// Show the dialog box.
+        /// </summary>
+        void Show();
+
+        /// <summary>
+        /// Cancel the dialog box.
+        /// </summary>
+        void Cancel();
+
+        /// <summary>
+        /// Suspend the coroutine,until the dialog box closed.
+        /// eg:
+        /// yiled return dialog.WaitForClosed();
+        /// </summary>
+        /// <returns></returns>
+        object WaitForClosed();
+
+        ///// <summary>
+        ///// Suspend the coroutine,until the dialog box closed.
+        ///// </summary>
+        ///// <typeparam name="TResult"></typeparam>
+        ///// <returns></returns>
+        //Task<TResult> WaitForClosed<TResult>();
+    }
+}
