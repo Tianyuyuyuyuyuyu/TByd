7b3502f1de8eb541586e7df0b8381872d261f18b
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:45:52 +0800

    Create ITargetProxyFactory.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Targets/ITargetProxyFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace TBydFramework.Runtime.Binding.Proxy.Targets
+{
+    public interface ITargetProxyFactory
+    {
+        ITargetProxy CreateProxy(object target, BindingDescription description);
+    }
+}
