183698a45252e961bd47eaf847bac0f11cae715b
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:40:09 +0800

    Create ParameterWrapBase.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Parameters/ParameterWrapBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Parameters
+{
+    public class ParameterWrapBase
+    {
+        protected readonly ICommandParameter commandParameter;
+        public ParameterWrapBase(ICommandParameter commandParameter)
+        {
+            if (commandParameter == null)
+                throw new ArgumentNullException("commandParameter");
+
+            this.commandParameter = commandParameter;
+        }
+
+        protected virtual object GetParameterValue()
+        {
+            return commandParameter.GetValue();
+        }
+
+        protected virtual Type GetParameterValueType()
+        {
+            return commandParameter.GetValueType();
+        }
+    }
+}
