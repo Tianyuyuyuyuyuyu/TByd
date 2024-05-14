391b806c0cb32b093a7c9803771535ea6cadf2fb
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:49:40 +0800

    Create IProxyMemberInfo.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Reflection/IProxyMemberInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Reflection
+{
+    public interface IProxyMemberInfo
+    {
+        Type DeclaringType { get; }
+
+        string Name { get; }
+
+        bool IsStatic { get; }
+    }
+}
