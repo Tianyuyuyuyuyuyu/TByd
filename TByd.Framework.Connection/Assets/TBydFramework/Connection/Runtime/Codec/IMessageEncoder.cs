e17596ddcca031259eca8ceff3baf2a35e7ee597
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 19:49:59 +0800

    Create IMessageEncoder.cs

---
+++ TByd.Framework.Connection/Assets/TBydFramework/Connection/Runtime/Codec/IMessageEncoder.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TBydFramework.Connection.Runtime.IO;
+
+namespace TBydFramework.Connection.Runtime.Codec
+{
+    public interface IMessageEncoder<TMessage>
+    {
+        Task Encode(IMessage message, BinaryWriter writer);
+    }
+}
