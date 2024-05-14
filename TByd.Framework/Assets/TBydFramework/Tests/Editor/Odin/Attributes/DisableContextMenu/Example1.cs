b75440b6638bde30518bb9c3c7db6fcf37393955
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:36:56 +0800

    Create Example1.cs

---
+++ TByd.Framework/Assets/TBydFramework/Tests/Editor/Odin/Attributes/DisableContextMenu/Example1.cs
@@ -0,0 +1,23 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace TBydFramework.Tests.Editor.Odin.Attributes.DisableContextMenu
+{
+    public class Example1 : MonoBehaviour
+    {
+        [InfoBox("禁用所有由Odin提供的右键上下文菜单。它不会禁用Unity的上下文菜单.", InfoMessageType.Warning)]
+        [DisableContextMenu]
+        public int[] NoRightClickList = new int[] { 2, 3, 5 };
+
+        [DisableContextMenu(disableForMember: false, disableCollectionElements: true)]
+        public int[] NoRightClickListOnListElements = new int[] { 7, 11 };
+
+        [DisableContextMenu(disableForMember: true, disableCollectionElements: true)]
+        public int[] DisableRightClickCompletely = new int[] { 13, 17 };
+
+        [DisableContextMenu]
+        public int NoRightClickField = 19;
+
+        public int RightClickField = 19;
+    }
+}
