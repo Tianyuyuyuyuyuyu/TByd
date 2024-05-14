ddb112a9c85753063feccae05acf1412f81dad94
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:00:11 +0800

    Create LocalizedText.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Localizations/UI/LocalizedText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TBydFramework.Runtime.Localizations.UI
+{
+    [AddComponentMenu("X/Localization/LocalizedText")]
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Text))]    
+    public class LocalizedText : AbstractLocalized<Text>
+    {
+        protected override void OnValueChanged(object sender, EventArgs e)
+        {
+            this.target.text = (string)Convert.ChangeType(this.value.Value, typeof(string));
+        }
+    }
+}
