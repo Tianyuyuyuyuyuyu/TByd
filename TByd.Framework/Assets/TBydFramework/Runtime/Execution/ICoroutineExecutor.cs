377b70f3f3fc149363e76517a57cc5ed0b46ba95
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:54:50 +0800

    Create ICoroutineExecutor.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Execution/ICoroutineExecutor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using TBydFramework.Runtime.Asynchronous;
+
+namespace TBydFramework.Runtime.Execution
+{
+    public interface ICoroutineExecutor
+    {
+        void RunOnCoroutineNoReturn(IEnumerator routine);
+
+        Asynchronous.IAsyncResult RunOnCoroutine(IEnumerator routine);
+
+        Asynchronous.IAsyncResult RunOnCoroutine(Func<Asynchronous.IPromise, IEnumerator> func);
+
+        IAsyncResult<TResult> RunOnCoroutine<TResult>(Func<Asynchronous.IPromise<TResult>, IEnumerator> func);
+
+        IProgressResult<TProgress> RunOnCoroutine<TProgress>(Func<IProgressPromise<TProgress>, IEnumerator> func);
+
+        IProgressResult<TProgress, TResult> RunOnCoroutine<TProgress, TResult>(Func<IProgressPromise<TProgress, TResult>, IEnumerator> func);
+    }
+}
