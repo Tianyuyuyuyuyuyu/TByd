b012a85ebac8431d5c85222f5214fc3d8fec8855
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:56:04 +0800

    Create AsyncInteractionEventArgs.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Interactivity/AsyncInteractionEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace TBydFramework.Runtime.Interactivity
+{
+    public class AsyncInteractionEventArgs : InteractionEventArgs
+    {
+        /// <summary>
+        /// Constructs a new instance of <see cref="AsyncInteractionEventArgs"/>
+        /// </summary>
+        /// <param name="context"></param>
+        public AsyncInteractionEventArgs(TaskCompletionSource<object> source, object context) : base(context, () => { source.TrySetResult(null); })
+        {
+            this.Source = source;
+        }
+
+        public TaskCompletionSource<object> Source { get; }
+    }
+}
