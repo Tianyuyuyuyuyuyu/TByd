d3ab1e3811fb0e6803eee0e58887bb02a960292b
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:17:25 +0800

    Create MonoSingletonPath.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Singleton/MonoSingletonPath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Singleton
+{
+    /// <summary>
+    /// MonoSingleton路径
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)] //这个特性只能标记在Class上
+    internal class MonoSingletonPath : System.Attribute
+    {
+        private string mPathInHierarchy;
+
+        public MonoSingletonPath(string pathInHierarchy)
+        {
+            mPathInHierarchy = pathInHierarchy;
+        }
+
+        public string PathInHierarchy
+        {
+            get { return mPathInHierarchy; }
+        }
+    }
+}
