d7e162b26eaac8c8ff53c9ede5f842ffe223744c
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 19:50:35 +0800

    Create IChannel.cs

---
+++ TByd.Framework.Connection/Assets/TBydFramework/Connection/Runtime/IChannel.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TBydFramework.Connection.Runtime
+{
+    public interface IChannel<T>
+    {
+        /// <summary>
+        /// Whether a connection has been established.
+        /// </summary>
+        bool Connected { get; }
+
+        /// <summary>
+        /// Connects the Client to the specified port on the specified host.
+        /// </summary>
+        /// <param name="hostname"></param>
+        /// <param name="port"></param>
+        /// <param name="timeoutMilliseconds"></param>
+        /// <returns></returns>
+        Task Connect(string hostname, int port, int timeoutMilliseconds);
+
+        /// <summary>
+        /// Connects the Client to the specified port on the specified host.
+        /// </summary>
+        /// <param name="hostname"></param>
+        /// <param name="port"></param>
+        /// <param name="timeoutMilliseconds"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task Connect(string hostname, int port, int timeoutMilliseconds, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Read a message asynchronously.
+        /// </summary>
+        /// <returns></returns>
+        Task<T> ReadAsync();
+
+        /// <summary>
+        /// Write a message asynchronously.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        Task WriteAsync(T message);
+
+        /// <summary>
+        ///  Forces a channel to close.
+        /// </summary>
+        /// <returns></returns>
+        Task Close();
+    }
+}
