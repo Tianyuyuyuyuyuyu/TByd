d14eb4be80d28ff241d5fcdf28a709225df08e00
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:59:21 +0800

    Create AllowedMembersAttribute.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Localizations/UI/AllowedMembersAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Localizations.UI
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class AllowedMembersAttribute : Attribute
+    {
+        private Type type;
+        private string[] names;
+
+        public AllowedMembersAttribute(Type type, params string[] names)
+        {
+            this.type = type;
+            this.names = names;
+            if (this.names == null)
+                this.names = new string[0];
+        }
+
+        public Type Type { get { return this.type; } }
+
+        public string[] Names { get { return this.names; } }
+    }
+}
