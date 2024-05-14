8fd432374219fce7430e1dadb956c24e3cbe7c5c
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:03:46 +0800

    Create MessageBase.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Messaging/MessageBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Messaging
+{
+    public class MessageBase : EventArgs
+    {
+        public MessageBase(object sender)
+        {
+            this.Sender = sender;
+        }
+
+        /// <summary>
+        /// Gets or sets the message's sender.
+        /// </summary>
+        public object Sender { get; protected set; }
+    }
+}
