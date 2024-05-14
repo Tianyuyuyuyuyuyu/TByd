764d3d888965ddeea5a2f5ba2939f86954e95b6e
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:02:40 +0800

    Create Level.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Log/Level.cs
@@ -0,0 +1,13 @@
+﻿namespace TBydFramework.Runtime.Log
+{
+    public enum Level
+    {
+        ALL = 0,
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        FATAL,
+        OFF
+    }
+}
