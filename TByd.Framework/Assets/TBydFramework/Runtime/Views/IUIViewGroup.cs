dc26d85d65dc504d7cd8ca863a6a79535f1ab0fd
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:27:03 +0800

    Create IUIViewGroup.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/IUIViewGroup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TBydFramework.Runtime.Views
+{
+    public delegate void UILayout(RectTransform transform);
+
+    public interface IUIViewGroup : IUIView
+    {
+        List<IUIView> Views { get; }
+
+        IUIView GetView(string name);
+
+        void AddView(IUIView view, bool worldPositionStays = false);
+
+        void AddView(IUIView view, UILayout layout);
+
+        void RemoveView(IUIView view, bool worldPositionStays = false);
+    }
+}
