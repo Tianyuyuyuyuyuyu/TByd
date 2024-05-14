f1478f1953484906d6a6542c4b1b18dc6f05bf14
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:43:53 +0800

    Create INodeProxyFactory.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Proxy/Sources/Object/INodeProxyFactory.cs
@@ -0,0 +1,9 @@
+﻿using TBydFramework.Runtime.Binding.Paths;
+
+namespace TBydFramework.Runtime.Binding.Proxy.Sources.Object
+{
+    public interface INodeProxyFactory
+    {
+        ISourceProxy Create(object source, PathToken token);
+    }
+}
