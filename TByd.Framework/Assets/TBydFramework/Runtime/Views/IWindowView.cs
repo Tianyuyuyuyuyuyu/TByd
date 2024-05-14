aa3eee5822978773a6c58730324dee498d980bb5
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:28:03 +0800

    Create IWindowView.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/IWindowView.cs
@@ -0,0 +1,17 @@
+﻿using TBydFramework.Runtime.Views.Animations;
+
+namespace TBydFramework.Runtime.Views
+{
+    public interface IWindowView : IUIViewGroup
+    {
+        /// <summary>
+        /// Activation animation
+        /// </summary>
+        IAnimation ActivationAnimation { get; set; }
+
+        /// <summary>
+        /// Passivation animation
+        /// </summary>
+        IAnimation PassivationAnimation { get; set; }
+    }
+}
