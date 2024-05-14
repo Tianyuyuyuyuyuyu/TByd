296259f43a2c5781d48ae3a343efc6da4d54275b
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:26:27 +0800

    Create ToastInteractionAction.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Views/InterationActions/ToastInteractionAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TBydFramework.Runtime.Interactivity;
+using TBydFramework.Runtime.Views.UI;
+
+namespace TBydFramework.Runtime.Views.InterationActions
+{
+    public class ToastInteractionAction : InteractionActionBase<ToastNotification>
+    {
+        private string viewName;
+        private IUIViewGroup viewGroup;
+
+        public ToastInteractionAction(IUIViewGroup viewGroup) : this(viewGroup, null)
+        {
+        }
+
+        public ToastInteractionAction(IUIViewGroup viewGroup, string viewName)
+        {
+            this.viewGroup = viewGroup;
+            this.viewName = viewName;
+        }
+
+        public override void Action(ToastNotification notification, Action callback)
+        {
+            if (notification == null)
+                return;
+
+            Toast.Show(this.viewName, this.viewGroup, notification.Message, notification.Duration, null, callback);
+        }
+    }
+}
