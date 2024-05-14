309cc35bedc7a33c584695f026f2d8742cf51b28
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:40:03 +0800

    Create ICommandParameter.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Parameters/ICommandParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Parameters
+{
+    public interface ICommandParameter
+    {
+        object GetValue();
+
+        Type GetValueType();
+    }
+
+    public interface ICommandParameter<T> : ICommandParameter
+    {
+        new T GetValue();
+    }
+}
