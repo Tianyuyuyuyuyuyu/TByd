db604153f43e38ec42e18e17a014362a021d4a14
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:13:26 +0800

    Create EnumResType.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Res/EnumResType.cs
@@ -0,0 +1,21 @@
+namespace TBydFramework.Runtime.Res
+{
+    /// <summary>
+    /// 资源类型枚举
+    /// </summary>
+    public enum EnumResType
+    {
+        /// <summary>
+        /// 未知
+        /// </summary>
+        UnKnown = 0,
+        /// <summary>
+        /// 预制体
+        /// </summary>
+        Prefab,
+        /// <summary>
+        /// 图片
+        /// </summary>
+        Texture,
+    }
+}
