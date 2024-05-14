0c779d845f6fbcc20363c713d3ae77deefd15130
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:06:07 +0800

    Create IMixedObjectPool.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/ObjectPool/IMixedObjectPool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.ObjectPool
+{
+    public interface IMixedObjectPool<T> : IDisposable where T : class
+    {
+        /// <summary>
+        /// Gets an object from the pool if one is available, otherwise creates one.
+        /// </summary>
+        /// <param name="typeName"></param>
+        /// <returns></returns>
+        T Allocate(string typeName);
+
+        /// <summary>
+        /// Return an object to the pool,if the number of objects in the pool is greater than or equal to the maximum value, the object is destroyed.
+        /// </summary>
+        /// <param name="typeName"></param>
+        /// <param name="obj"></param>
+        void Free(string typeName, T obj);
+    }
+}
