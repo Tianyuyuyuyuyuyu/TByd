64b6bdcee2dd141adaec509c8d4f1e6f6e624a91
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:54:11 +0800

    Create CoroutineExecutor.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Execution/CoroutineExecutor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using TBydFramework.Runtime.Asynchronous;
+
+namespace TBydFramework.Runtime.Execution
+{
+    public class CoroutineExecutor : AbstractExecutor, ICoroutineExecutor
+    {
+        public virtual void RunOnCoroutineNoReturn(IEnumerator routine)
+        {
+            Executors.RunOnCoroutineNoReturn(routine);
+        }
+
+        public virtual Asynchronous.IAsyncResult RunOnCoroutine(IEnumerator routine)
+        {
+            return Executors.RunOnCoroutine(routine);
+        }
+
+        public virtual Asynchronous.IAsyncResult RunOnCoroutine(Func<Asynchronous.IPromise, IEnumerator> func)
+        {
+            return Executors.RunOnCoroutine(func);
+        }
+
+        public virtual IAsyncResult<TResult> RunOnCoroutine<TResult>(Func<Asynchronous.IPromise<TResult>, IEnumerator> func)
+        {
+            return Executors.RunOnCoroutine(func);
+        }
+
+        public virtual IProgressResult<TProgress> RunOnCoroutine<TProgress>(Func<IProgressPromise<TProgress>, IEnumerator> func)
+        {
+            return Executors.RunOnCoroutine(func);
+        }
+
+        public virtual IProgressResult<TProgress, TResult> RunOnCoroutine<TProgress, TResult>(Func<IProgressPromise<TProgress, TResult>, IEnumerator> func)
+        {
+            return Executors.RunOnCoroutine(func);
+        }
+    }
+}
