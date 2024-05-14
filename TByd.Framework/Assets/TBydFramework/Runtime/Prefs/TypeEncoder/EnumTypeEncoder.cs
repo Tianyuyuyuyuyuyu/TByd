43c49997f36d0be0c8970e9ac898e21578862da4
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 12:12:08 +0800

    Create EnumTypeEncoder.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Prefs/TypeEncoder/EnumTypeEncoder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Prefs.TypeEncoder
+{
+
+    public class EnumTypeEncoder : ITypeEncoder
+    {
+        private int priority = 998;
+
+        public int Priority
+        {
+            get { return this.priority; }
+            set { this.priority = value; }
+        }
+
+        public bool IsSupport(Type type)
+        {
+            if (type.IsEnum)
+                return true;
+            return false;
+        }
+
+        public object Decode(Type type, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return null;
+
+            return Enum.Parse(type, value);
+        }
+
+        public string Encode(object value)
+        {
+            return Enum.GetName(value.GetType(), value);
+        }
+    }
+}
