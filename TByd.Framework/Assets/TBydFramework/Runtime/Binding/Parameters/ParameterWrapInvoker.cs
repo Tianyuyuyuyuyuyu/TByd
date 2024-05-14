864d334864aad11bad9f9708be3c7e8d764062d1
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-14 11:40:33 +0800

    Create ParameterWrapInvoker.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Binding/Parameters/ParameterWrapInvoker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TBydFramework.Runtime.Binding.Parameters
+{
+    public class ParameterWrapInvoker : IInvoker
+    {
+        protected readonly IInvoker invoker;
+        protected readonly ICommandParameter commandParameter;
+        public ParameterWrapInvoker(IInvoker invoker, ICommandParameter commandParameter)
+        {
+            if (invoker == null)
+                throw new ArgumentNullException("invoker");
+
+            if (commandParameter == null)
+                throw new ArgumentNullException("commandParameter");
+
+            this.invoker = invoker;
+            this.commandParameter = commandParameter;
+        }
+
+        public object Invoke(params object[] args)
+        {
+            return this.invoker.Invoke(commandParameter.GetValue());
+        }
+    }
+
+    public class ParameterWrapInvoker<T> : IInvoker
+    {
+        protected readonly IInvoker<T> invoker;
+        protected readonly ICommandParameter<T> commandParameter;
+        public ParameterWrapInvoker(IInvoker<T> invoker, ICommandParameter<T> commandParameter)
+        {
+            if (invoker == null)
+                throw new ArgumentNullException("invoker");
+
+            if (commandParameter == null)
+                throw new ArgumentNullException("commandParameter");
+
+            this.invoker = invoker;
+            this.commandParameter = commandParameter;
+        }
+
+        public object Invoke(params object[] args)
+        {
+            return this.invoker.Invoke(commandParameter.GetValue());
+        }
+    }
+}
