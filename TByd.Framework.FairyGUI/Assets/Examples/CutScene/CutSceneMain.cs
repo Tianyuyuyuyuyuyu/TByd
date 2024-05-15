8f6604418f93ce9d8d39e03202c16b9d54e9b610
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-15 12:02:38 +0800

    Create CutSceneMain.cs

---
+++ TByd.Framework.FairyGUI/Assets/Examples/CutScene/CutSceneMain.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using TBydFramework.FairyGUI.Runtime.Core;
+using TBydFramework.FairyGUI.Runtime.Event;
+using TBydFramework.FairyGUI.Runtime.UI;
+using UnityEngine;
+
+/// <summary>
+/// Demonstrated the simple flow of a game.
+/// </summary>
+public class CutSceneMain : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        Stage.inst.onKeyDown.Add(OnKeyDown);
+
+        UIPackage.AddPackage("UI/CutScene");
+
+        LevelManager.inst.Init();
+        LevelManager.inst.LoadLevel("scene1");
+    }
+
+    void OnKeyDown(EventContext context)
+    {
+        if (context.inputEvent.keyCode == KeyCode.Escape)
+        {
+            Application.Quit();
+        }
+    }
+}
