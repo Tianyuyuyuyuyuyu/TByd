021f800a98ec477f9126380a20676df4a22c3ba1
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-15 12:44:41 +0800

    Create StageCameraEditor.cs

---
+++ TByd.Framework.FairyGUI/Assets/TBydFramework/FairyGUI/Editor/StageCameraEditor.cs
@@ -0,0 +1,29 @@
+﻿using TBydFramework.FairyGUI.Runtime.Core;
+using UnityEditor;
+
+namespace TBydFramework.FairyGUI.Editor
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CustomEditor(typeof(StageCamera))]
+    public class StageCameraEditor : UnityEditor.Editor
+    {
+        string[] propertyToExclude;
+
+        void OnEnable()
+        {
+            propertyToExclude = new string[] { "m_Script" };
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawPropertiesExcluding(serializedObject, propertyToExclude);
+
+            if (serializedObject.ApplyModifiedProperties())
+                (target as StageCamera).ApplyModifiedProperties();
+        }
+    }
+}
