f458f9588dc92d32236338162a83473aa3d8a242
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-15 12:56:24 +0800

    Create IFilter.cs

---
+++ TByd.Framework.FairyGUI/Assets/TBydFramework/FairyGUI/Runtime/Filter/IFilter.cs
@@ -0,0 +1,22 @@
+﻿using TBydFramework.FairyGUI.Runtime.Core;
+
+namespace TBydFramework.FairyGUI.Runtime.Filter
+{
+    public interface IFilter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        DisplayObject target { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Update();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Dispose();
+    }
+}
