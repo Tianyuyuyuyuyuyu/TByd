a228ef5a1e26d493bf5fbb1faf626b3e273cc0b7
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-15 12:04:15 +0800

    Create MailItem.cs

---
+++ TByd.Framework.FairyGUI/Assets/Examples/Extension/MailItem.cs
@@ -0,0 +1,42 @@
+﻿using TBydFramework.FairyGUI.Runtime.UI;
+using TBydFramework.FairyGUI.Runtime.Utils;
+using UnityEngine;
+
+public class MailItem : GButton
+{
+    GTextField _timeText;
+    Controller _readController;
+    Controller _fetchController;
+    Transition _trans;
+
+    public override void ConstructFromXML(XML cxml)
+    {
+        base.ConstructFromXML(cxml);
+
+        _timeText = this.GetChild("timeText").asTextField;
+        _readController = this.GetController("IsRead");
+        _fetchController = this.GetController("c1");
+        _trans = this.GetTransition("t0");
+    }
+
+    public void setTime(string value)
+    {
+        _timeText.text = value;
+    }
+
+    public void setRead(bool value)
+    {
+        _readController.selectedIndex = value ? 1 : 0;
+    }
+
+    public void setFetched(bool value)
+    {
+        _fetchController.selectedIndex = value ? 1 : 0;
+    }
+
+    public void PlayEffect(float delay)
+    {
+        this.visible = false;
+        _trans.Play(1, delay, null);
+    }
+}
