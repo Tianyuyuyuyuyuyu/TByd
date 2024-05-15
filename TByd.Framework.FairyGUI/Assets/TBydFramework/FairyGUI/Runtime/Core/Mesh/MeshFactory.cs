d01876724291e3da1e29ca61e31dd0ab8370e82c
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-15 12:50:11 +0800

    Create MeshFactory.cs

---
+++ TByd.Framework.FairyGUI/Assets/TBydFramework/FairyGUI/Runtime/Core/Mesh/MeshFactory.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TBydFramework.FairyGUI.Runtime.Core.Mesh
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IMeshFactory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vb"></param>
+        void OnPopulateMesh(VertexBuffer vb);
+    }
+}
