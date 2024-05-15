751f063478a0a68d700c230a90d40ac85245541f
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-15 13:13:00 +0800

    TByd.Framework.FairyGUI

---
+++ TByd.Framework.FairyGUI/Assets/TBydFramework/FairyGUI/Runtime/UI/Gears/IColorGear.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace TBydFramework.FairyGUI.Runtime.UI.Gears
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IColorGear
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Color color { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ITextColorGear : IColorGear
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Color strokeColor { get; set; }
+    }
+}
---
+++ TByd.Framework.FairyGUI/Assets/TBydFramework/FairyGUI/Runtime/Utils/Html/IHtmlPageContext.cs
@@ -0,0 +1,18 @@
+﻿
+using TBydFramework.FairyGUI.Runtime.Core;
+using TBydFramework.FairyGUI.Runtime.Core.Text;
+
+namespace TBydFramework.FairyGUI.Runtime.Utils.Html
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IHtmlPageContext
+    {
+        IHtmlObject CreateObject(RichTextField owner, HtmlElement element);
+        void FreeObject(IHtmlObject obj);
+
+        NTexture GetImageTexture(HtmlImage image);
+        void FreeImageTexture(HtmlImage image, NTexture texture);
+    }
+}
