0463700c11280c44f7f4b71c72d80df332909eac
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-15 12:06:44 +0800

    Create CoolComponent.cs

---
+++ TByd.Framework.FairyGUI/Assets/Examples/Particles/CoolComponent.cs
@@ -0,0 +1,18 @@
+﻿using TBydFramework.FairyGUI.Runtime.Core;
+using TBydFramework.FairyGUI.Runtime.UI;
+using TBydFramework.FairyGUI.Runtime.Utils;
+using UnityEngine;
+
+public class CoolComponent : GComponent
+{
+    public override void ConstructFromXML(XML cxml)
+    {
+        base.ConstructFromXML(cxml);
+
+        GGraph graph = this.GetChild("effect").asGraph;
+
+        Object prefab = Resources.Load("Flame");
+        GameObject go = (GameObject)Object.Instantiate(prefab);
+        graph.SetNativeObject(new GoWrapper(go));
+    }
+}
