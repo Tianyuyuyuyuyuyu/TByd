9052e0c61eb8c6d21e2cc52f4f2c0cb303c11b1d
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-15 12:52:31 +0800

    Create StageEngine.cs

---
+++ TByd.Framework.FairyGUI/Assets/TBydFramework/FairyGUI/Runtime/Core/StageEngine.cs
@@ -0,0 +1,50 @@
+﻿using TBydFramework.FairyGUI.Runtime.UI;
+using UnityEngine;
+
+namespace TBydFramework.FairyGUI.Runtime.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StageEngine : MonoBehaviour
+    {
+        public int ObjectsOnStage;
+        public int GraphicsOnStage;
+
+        public static bool beingQuit;
+
+        void Start()
+        {
+            useGUILayout = false;
+        }
+
+        void LateUpdate()
+        {
+            Stage.inst.InternalUpdate();
+
+            ObjectsOnStage = Stats.ObjectCount;
+            GraphicsOnStage = Stats.GraphicsCount;
+        }
+
+        void OnGUI()
+        {
+            Stage.inst.HandleGUIEvents(UnityEngine.Event.current);
+        }
+
+#if !UNITY_5_4_OR_NEWER
+        void OnLevelWasLoaded()
+        {
+            StageCamera.CheckMainCamera();
+        }
+#endif
+
+        void OnApplicationQuit()
+        {
+            if (Application.isEditor)
+            {
+                beingQuit = true;
+                UIPackage.RemoveAllPackages();
+            }
+        }
+    }
+}
