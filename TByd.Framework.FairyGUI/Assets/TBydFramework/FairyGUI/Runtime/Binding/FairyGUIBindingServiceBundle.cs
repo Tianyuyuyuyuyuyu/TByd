43e3d5fe43b6d7c6d8d4877e8d8ad7d16e5ebcdd
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-15 12:46:31 +0800

    Create FairyGUIBindingServiceBundle.cs

---
+++ TByd.Framework.FairyGUI/Assets/TBydFramework/FairyGUI/Runtime/Binding/FairyGUIBindingServiceBundle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TBydFramework.FairyGUI.Runtime.Binding.Proxy;
+using TBydFramework.Runtime.Binding.Proxy.Targets;
+using TBydFramework.Runtime.Services;
+
+namespace TBydFramework.FairyGUI.Runtime.Binding
+{
+    public class FairyGUIBindingServiceBundle : AbstractServiceBundle
+    {
+        public FairyGUIBindingServiceBundle(IServiceContainer container) : base(container)
+        {
+        }
+
+        protected override void OnStart(IServiceContainer container)
+        {
+            var targetFactory = container.Resolve<ITargetProxyFactoryRegister>();
+            if (targetFactory == null)
+                throw new Exception("Data binding service is not initialized,please create a BindingServiceBundle service before using it.");
+
+            targetFactory.Register(new FairyTargetProxyFactory(), 20);
+        }
+
+        protected override void OnStop(IServiceContainer container)
+        {
+        }
+    }
+}
