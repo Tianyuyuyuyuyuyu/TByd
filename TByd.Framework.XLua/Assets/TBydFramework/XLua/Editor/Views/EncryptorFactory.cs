b0fbc666cb079a22008736299c2065a0fed339bf
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-16 17:42:51 +0800

    Create EncryptorFactory.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Editor/Views/EncryptorFactory.cs
@@ -0,0 +1,10 @@
+﻿using TBydFramework.Runtime.Security.Cryptography;
+using UnityEngine;
+
+namespace TBydFramework.XLua.Editor.Views
+{
+    public abstract class EncryptorFactory : ScriptableObject
+    {
+        public abstract IEncryptor Create();
+    }
+}
