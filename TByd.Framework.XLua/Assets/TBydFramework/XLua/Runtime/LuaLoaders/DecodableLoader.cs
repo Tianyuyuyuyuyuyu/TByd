3f7425bd18cfa499362bcf65d4ed990e2c90bade
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:27:35 +0800

    Create DecodableLoader.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/LuaLoaders/DecodableLoader.cs
@@ -0,0 +1,25 @@
+﻿using TBydFramework.Runtime.Security.Cryptography;
+using XLua;
+
+namespace TBydFramework.XLua.Runtime.LuaLoaders
+{
+    public class DecodableLoader : LoaderBase
+    {
+        private IDecryptor decryptor;
+        private LuaEnv.CustomLoader loader;
+
+        public DecodableLoader(LuaEnv.CustomLoader loader, IDecryptor decryptor)
+        {
+            this.decryptor = decryptor;
+            this.loader = loader;
+        }
+
+        protected override byte[] Load(ref string path)
+        {
+            byte[] data = this.loader(ref path);
+            if (data == null)
+                return null;
+            return this.decryptor.Decrypt(data);
+        }
+    }
+}
