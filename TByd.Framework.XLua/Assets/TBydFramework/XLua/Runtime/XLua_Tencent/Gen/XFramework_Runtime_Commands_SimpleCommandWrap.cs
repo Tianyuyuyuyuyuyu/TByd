848f28d14935e66d93c3e93d7a0cefdf29559377
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:54:51 +0800

    Create XFramework_Runtime_Commands_SimpleCommandWrap.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/XLua_Tencent/Gen/XFramework_Runtime_Commands_SimpleCommandWrap.cs
@@ -0,0 +1,185 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class XFrameworkRuntimeCommandsSimpleCommandWrap 
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			System.Type type = typeof(TBydFramework.Runtime.Commands.SimpleCommand);
+			Utils.BeginObjectRegister(type, L, translator, 0, 2, 1, 1);
+			
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "CanExecute", _m_CanExecute);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "Execute", _m_Execute);
+			
+			
+			Utils.RegisterFunc(L, Utils.GETTER_IDX, "Enabled", _g_get_Enabled);
+            
+			Utils.RegisterFunc(L, Utils.SETTER_IDX, "Enabled", _s_set_Enabled);
+            
+			
+			Utils.EndObjectRegister(type, L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(type, L, __CreateInstance, 1, 0, 0);
+			
+			
+            
+			
+			
+			
+			Utils.EndClassRegister(type, L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            
+			try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+				if(LuaAPI.lua_gettop(L) == 3 && translator.Assignable<System.Action>(L, 2) && LuaTypes.LUA_TBOOLEAN == LuaAPI.lua_type(L, 3))
+				{
+					System.Action _execute = translator.GetDelegate<System.Action>(L, 2);
+					bool _keepStrongRef = LuaAPI.lua_toboolean(L, 3);
+					
+					var gen_ret = new TBydFramework.Runtime.Commands.SimpleCommand(_execute, _keepStrongRef);
+					translator.Push(L, gen_ret);
+                    
+					return 1;
+				}
+				if(LuaAPI.lua_gettop(L) == 2 && translator.Assignable<System.Action>(L, 2))
+				{
+					System.Action _execute = translator.GetDelegate<System.Action>(L, 2);
+					
+					var gen_ret = new TBydFramework.Runtime.Commands.SimpleCommand(_execute);
+					translator.Push(L, gen_ret);
+                    
+					return 1;
+				}
+				
+			}
+			catch(System.Exception gen_e) {
+				return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+			}
+            return LuaAPI.luaL_error(L, "invalid arguments to TBydFramework.Runtime.Commands.SimpleCommand constructor!");
+            
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_CanExecute(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                TBydFramework.Runtime.Commands.SimpleCommand gen_to_be_invoked = (TBydFramework.Runtime.Commands.SimpleCommand)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    object _parameter = translator.GetObject(L, 2, typeof(object));
+                    
+                        var gen_ret = gen_to_be_invoked.CanExecute( _parameter );
+                        LuaAPI.lua_pushboolean(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_Execute(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                TBydFramework.Runtime.Commands.SimpleCommand gen_to_be_invoked = (TBydFramework.Runtime.Commands.SimpleCommand)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    object _parameter = translator.GetObject(L, 2, typeof(object));
+                    
+                    gen_to_be_invoked.Execute( _parameter );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_Enabled(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                TBydFramework.Runtime.Commands.SimpleCommand gen_to_be_invoked = (TBydFramework.Runtime.Commands.SimpleCommand)translator.FastGetCSObj(L, 1);
+                LuaAPI.lua_pushboolean(L, gen_to_be_invoked.Enabled);
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 1;
+        }
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _s_set_Enabled(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                TBydFramework.Runtime.Commands.SimpleCommand gen_to_be_invoked = (TBydFramework.Runtime.Commands.SimpleCommand)translator.FastGetCSObj(L, 1);
+                gen_to_be_invoked.Enabled = LuaAPI.lua_toboolean(L, 2);
+            
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 0;
+        }
+        
+		
+		
+		
+		
+    }
+}
