3aea3d29f5dbf9e5658a03ad75d8ce579b38abef
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:56:38 +0800

    Create XFramework_Runtime_Views_ITransitionWrap.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/XLua_Tencent/Gen/XFramework_Runtime_Views_ITransitionWrap.cs
@@ -0,0 +1,320 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class XFrameworkRuntimeViewsITransitionWrap 
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			System.Type type = typeof(TBydFramework.Runtime.Views.ITransition);
+			Utils.BeginObjectRegister(type, L, translator, 0, 8, 1, 0);
+			
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "WaitForDone", _m_WaitForDone);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "GetAwaiter", _m_GetAwaiter);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "DisableAnimation", _m_DisableAnimation);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "AtLayer", _m_AtLayer);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "Overlay", _m_Overlay);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "OnStart", _m_OnStart);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "OnStateChanged", _m_OnStateChanged);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "OnFinish", _m_OnFinish);
+			
+			
+			Utils.RegisterFunc(L, Utils.GETTER_IDX, "IsDone", _g_get_IsDone);
+            
+			
+			
+			Utils.EndObjectRegister(type, L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(type, L, __CreateInstance, 1, 0, 0);
+			
+			
+            
+			
+			
+			
+			Utils.EndClassRegister(type, L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            return LuaAPI.luaL_error(L, "TBydFramework.Runtime.Views.ITransition does not have a constructor!");
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_WaitForDone(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                TBydFramework.Runtime.Views.ITransition gen_to_be_invoked = (TBydFramework.Runtime.Views.ITransition)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    
+                        var gen_ret = gen_to_be_invoked.WaitForDone(  );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_GetAwaiter(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                TBydFramework.Runtime.Views.ITransition gen_to_be_invoked = (TBydFramework.Runtime.Views.ITransition)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    
+                        var gen_ret = gen_to_be_invoked.GetAwaiter(  );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_DisableAnimation(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                TBydFramework.Runtime.Views.ITransition gen_to_be_invoked = (TBydFramework.Runtime.Views.ITransition)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    bool _disabled = LuaAPI.lua_toboolean(L, 2);
+                    
+                        var gen_ret = gen_to_be_invoked.DisableAnimation( _disabled );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_AtLayer(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                TBydFramework.Runtime.Views.ITransition gen_to_be_invoked = (TBydFramework.Runtime.Views.ITransition)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    int _layer = LuaAPI.xlua_tointeger(L, 2);
+                    
+                        var gen_ret = gen_to_be_invoked.AtLayer( _layer );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_Overlay(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                TBydFramework.Runtime.Views.ITransition gen_to_be_invoked = (TBydFramework.Runtime.Views.ITransition)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    System.Func<TBydFramework.Runtime.Views.IWindow, TBydFramework.Runtime.Views.IWindow, TBydFramework.Runtime.Views.ActionType> _policy = translator.GetDelegate<System.Func<TBydFramework.Runtime.Views.IWindow, TBydFramework.Runtime.Views.IWindow, TBydFramework.Runtime.Views.ActionType>>(L, 2);
+                    
+                        var gen_ret = gen_to_be_invoked.Overlay( _policy );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_OnStart(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                TBydFramework.Runtime.Views.ITransition gen_to_be_invoked = (TBydFramework.Runtime.Views.ITransition)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    System.Action _callback = translator.GetDelegate<System.Action>(L, 2);
+                    
+                        var gen_ret = gen_to_be_invoked.OnStart( _callback );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_OnStateChanged(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                TBydFramework.Runtime.Views.ITransition gen_to_be_invoked = (TBydFramework.Runtime.Views.ITransition)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    System.Action<TBydFramework.Runtime.Views.IWindow, TBydFramework.Runtime.Views.WindowState> _callback = translator.GetDelegate<System.Action<TBydFramework.Runtime.Views.IWindow, TBydFramework.Runtime.Views.WindowState>>(L, 2);
+                    
+                        var gen_ret = gen_to_be_invoked.OnStateChanged( _callback );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_OnFinish(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                TBydFramework.Runtime.Views.ITransition gen_to_be_invoked = (TBydFramework.Runtime.Views.ITransition)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    System.Action _callback = translator.GetDelegate<System.Action>(L, 2);
+                    
+                        var gen_ret = gen_to_be_invoked.OnFinish( _callback );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_IsDone(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			
+                TBydFramework.Runtime.Views.ITransition gen_to_be_invoked = (TBydFramework.Runtime.Views.ITransition)translator.FastGetCSObj(L, 1);
+                LuaAPI.lua_pushboolean(L, gen_to_be_invoked.IsDone);
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 1;
+        }
+        
+        
+        
+		
+		
+		
+		
+    }
+}
