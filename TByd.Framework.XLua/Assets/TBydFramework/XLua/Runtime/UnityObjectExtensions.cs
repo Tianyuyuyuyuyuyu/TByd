6a26c4d25249e3c0f48f762894a5b05de5da0b30
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:28:20 +0800

    Create UnityObjectExtensions.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/UnityObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using XLua;
+
+namespace TBydFramework.XLua.Runtime
+{
+    [LuaCallCSharp]
+    [ReflectionUse]
+    public static class UnityObjectExtensions
+    {
+        public static bool IsDestroyed(this Object o)
+        {
+            return o == null;
+        }
+    }
+}
