272195e00145cd8288ae7966bfc637558e8b2acb
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:59:09 +0800

    Create XFramework_XLua_Runtime_Views_Animations_GenericUIAnimationWrap.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/XLua_Tencent/Gen/XFramework_XLua_Runtime_Views_Animations_GenericUIAnimationWrap.cs
@@ -0,0 +1,176 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+using TBydFramework.XLua.Runtime.Views.Animations;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class XFrameworkXLuaRuntimeViewsAnimationsGenericUIAnimationWrap 
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			System.Type type = typeof(GenericUIAnimation);
+			Utils.BeginObjectRegister(type, L, translator, 0, 3, 0, 0);
+			
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "OnStart", _m_OnStart);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "OnEnd", _m_OnEnd);
+			Utils.RegisterFunc(L, Utils.METHOD_IDX, "Play", _m_Play);
+			
+			
+			
+			
+			
+			Utils.EndObjectRegister(type, L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(type, L, __CreateInstance, 1, 0, 0);
+			
+			
+            
+			
+			
+			
+			Utils.EndClassRegister(type, L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            
+			try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+				if(LuaAPI.lua_gettop(L) == 3 && translator.Assignable<TBydFramework.Runtime.Views.IUIView>(L, 2) && translator.Assignable<AnimationAction>(L, 3))
+				{
+					TBydFramework.Runtime.Views.IUIView _view = (TBydFramework.Runtime.Views.IUIView)translator.GetObject(L, 2, typeof(TBydFramework.Runtime.Views.IUIView));
+					AnimationAction _animation = translator.GetDelegate<AnimationAction>(L, 3);
+					
+					var gen_ret = new GenericUIAnimation(_view, _animation);
+					translator.Push(L, gen_ret);
+                    
+					return 1;
+				}
+				
+			}
+			catch(System.Exception gen_e) {
+				return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+			}
+            return LuaAPI.luaL_error(L, "invalid arguments to TBydFramework.XLua.Runtime.Views.Animations.GenericUIAnimation constructor!");
+            
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_OnStart(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                GenericUIAnimation gen_to_be_invoked = (GenericUIAnimation)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    System.Action _onStart = translator.GetDelegate<System.Action>(L, 2);
+                    
+                        var gen_ret = gen_to_be_invoked.OnStart( _onStart );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_OnEnd(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                GenericUIAnimation gen_to_be_invoked = (GenericUIAnimation)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    System.Action _onEnd = translator.GetDelegate<System.Action>(L, 2);
+                    
+                        var gen_ret = gen_to_be_invoked.OnEnd( _onEnd );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_Play(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+                GenericUIAnimation gen_to_be_invoked = (GenericUIAnimation)translator.FastGetCSObj(L, 1);
+            
+            
+                
+                {
+                    
+                        var gen_ret = gen_to_be_invoked.Play(  );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        
+        
+        
+        
+        
+		
+		
+		
+		
+    }
+}
