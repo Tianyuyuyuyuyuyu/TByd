758b675b5d1919c4ba83d98d0a9dd082479fb937
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:27:53 +0800

    Create LoaderBase.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/LuaLoaders/LoaderBase.cs
@@ -0,0 +1,14 @@
+﻿using XLua;
+
+namespace TBydFramework.XLua.Runtime.LuaLoaders
+{
+    public abstract class LoaderBase
+    {
+        protected abstract byte[] Load(ref string path);
+
+        public static implicit operator LuaEnv.CustomLoader(LoaderBase loader)
+        {
+            return loader.Load;
+        }
+    }
+}
