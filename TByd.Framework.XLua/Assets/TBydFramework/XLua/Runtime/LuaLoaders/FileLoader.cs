474ce3c1897028b76590e5b94c095cb5686a1df8
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:27:44 +0800

    Create FileLoader.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/LuaLoaders/FileLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using TBydFramework.Runtime.Utilities;
+
+namespace TBydFramework.XLua.Runtime.LuaLoaders
+{
+    public class FileLoader : PathLoaderBase
+    {
+        public FileLoader(string prefix) : this(prefix, ".lua.txt")
+        {
+        }
+
+        public FileLoader(string prefix, string suffix) : base(prefix, suffix)
+        {
+        }
+
+        protected override byte[] Load(ref string path)
+        {
+            string fullname = GetFullname(path);
+            if (!FileUtil.Exists(fullname))
+                return null;
+
+            path = fullname;
+            byte[] data = FileUtil.ReadAllBytes(fullname);
+            if (!HasBOMFlag(data))
+                return data;
+
+            return data.Skip(3).ToArray();
+        }
+
+        protected bool HasBOMFlag(byte[] data)
+        {
+            if (data == null || data.Length < 3)
+                return false;
+
+            if (data[0] == 239 && data[1] == 187 && data[2] == 191)
+                return true;
+
+            return false;
+        }
+    }
+}
