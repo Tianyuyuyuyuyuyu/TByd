275df0f61f0331c6a1b19578132ab47700d0a5ce
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:25:21 +0800

    Create LuaExpressionSourceDescription.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/Binding/Proxy/Sources/Expressions/LuaExpressionSourceDescription.cs
@@ -0,0 +1,38 @@
+﻿using TBydFramework.Runtime.Binding.Proxy.Sources;
+using XLua;
+
+namespace TBydFramework.XLua.Runtime.Binding.Proxy.Sources.Expressions
+{
+    public class LuaExpressionSourceDescription : SourceDescription
+    {
+        private LuaFunction expression;
+
+        private string[] paths;
+
+        public LuaExpressionSourceDescription() : this(false)
+        {
+        }
+
+        public LuaExpressionSourceDescription(bool isStatic)
+        {
+            this.IsStatic = isStatic;
+        }
+
+        public LuaFunction Expression
+        {
+            get { return this.expression; }
+            set { this.expression = value; }
+        }
+
+        public string[] Paths
+        {
+            get { return this.paths; }
+            set { this.paths = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.expression == null ? "Expression:null" : "Expression:" + this.expression.ToString();
+        }
+    }
+}
