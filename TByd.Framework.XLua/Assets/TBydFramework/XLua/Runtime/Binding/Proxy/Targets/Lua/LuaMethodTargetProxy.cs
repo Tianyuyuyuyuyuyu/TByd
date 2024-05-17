726e7357999390f8f8b677f578f0e68ee9d4ef95
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:26:37 +0800

    Create LuaMethodTargetProxy.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/Binding/Proxy/Targets/Lua/LuaMethodTargetProxy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using TBydFramework.Runtime.Binding;
+using TBydFramework.Runtime.Binding.Proxy;
+using TBydFramework.Runtime.Binding.Proxy.Targets;
+using XLua;
+
+namespace TBydFramework.XLua.Runtime.Binding.Proxy.Targets.Lua
+{
+    public class LuaMethodTargetProxy : TargetProxyBase, IObtainable
+    {
+        private bool disposed = false;
+        private IScriptInvoker invoker;
+
+        public LuaMethodTargetProxy(object target, LuaFunction function) : base(target)
+        {
+            if (target == null)
+                throw new ArgumentNullException("target", "Unable to bind to target as it's null");
+
+            this.invoker = new LuaInvoker(target, function);
+        }
+
+        public override BindingMode DefaultMode { get { return BindingMode.OneWayToSource; } }
+
+        public override Type Type { get { return typeof(LuaFunction); } }
+
+        public object GetValue()
+        {
+            return this.invoker;
+        }
+
+        public TValue GetValue<TValue>()
+        {
+            return (TValue)this.invoker;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (invoker != null && invoker is IDisposable)
+                    {
+                        (invoker as IDisposable).Dispose();
+                        invoker = null;
+                    }
+                }
+                disposed = true;
+                base.Dispose(disposing);
+            }
+        }
+    }
+}
