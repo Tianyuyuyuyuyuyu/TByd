f2f34dace4b4d8e0285d91b2916114943ec32f15
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:28:02 +0800

    Create PathLoaderBase.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/LuaLoaders/PathLoaderBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TBydFramework.XLua.Runtime.LuaLoaders
+{
+    public abstract class PathLoaderBase : LoaderBase, IDisposable
+    {
+        protected string prefix = "";
+        protected string suffix = ".lua.txt";
+
+        public PathLoaderBase(string prefix, string suffix)
+        {
+            this.prefix = prefix;
+            if (!string.IsNullOrEmpty(this.prefix))
+                this.prefix = this.prefix.Replace(@"\", "/");
+            if (!this.prefix.EndsWith("/"))
+                this.prefix += "/";
+
+            this.suffix = suffix;
+        }
+
+        protected virtual string GetFullname(string className)
+        {
+            return string.Format("{0}{1}{2}", prefix, className.Replace(".", "/"), suffix);
+        }
+
+        #region IDisposable Support
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+        ~PathLoaderBase()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
