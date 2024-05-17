996d349fccf17b88fc3c971eec88d70c39fca693
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:23:24 +0800

    Create ILuaTask.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/Asynchronous/ILuaTask.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using XLua;
+
+namespace TBydFramework.XLua.Runtime.Asynchronous
+{
+    [CSharpCallLua]
+    public interface ILuaTask
+    {
+        /// <summary>
+        /// Gets the result of the asynchronous operation.
+        /// </summary>
+        object GetResult();
+
+        /// <summary>
+        /// Gets the cause of the asynchronous operation.
+        /// </summary>
+        string GetException();
+
+        /// <summary>
+        /// Returns <code>true</code> if the asynchronous operation is finished.
+        /// </summary>
+        bool IsCompleted { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="continuation"></param>
+        void OnCompleted(Action continuation);
+    }
+
+    [CSharpCallLua]
+    public interface ILuaTask<TResult>
+    {
+        /// <summary>
+        /// Gets the result of the asynchronous operation.
+        /// </summary>
+        TResult GetResult();
+
+        /// <summary>
+        /// Gets the cause of the asynchronous operation.
+        /// </summary>
+        string GetException();
+
+        /// <summary>
+        /// Returns <code>true</code> if the asynchronous operation is finished.
+        /// </summary>
+        bool IsCompleted { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="continuation"></param>
+        void OnCompleted(Action continuation);
+    }
+}
