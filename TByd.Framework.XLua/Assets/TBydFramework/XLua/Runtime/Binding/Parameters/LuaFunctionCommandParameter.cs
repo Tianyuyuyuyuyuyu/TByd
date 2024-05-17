080f3b4ee3f4dc97db76884b8d33c36b09081020
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:24:49 +0800

    Create LuaFunctionCommandParameter.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/Binding/Parameters/LuaFunctionCommandParameter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TBydFramework.Runtime.Binding.Parameters;
+using XLua;
+
+namespace TBydFramework.XLua.Runtime.Binding.Parameters
+{
+    public class LuaFunctionCommandParameter : ICommandParameter
+    {
+        private LuaFunction function;
+        public LuaFunctionCommandParameter(LuaFunction function)
+        {
+            this.function = function;
+        }
+        public object GetValue()
+        {
+            object[] results = function.Call();
+            if (results == null || results.Length <= 0)
+                return null;
+            return results[0];
+        }
+
+        public Type GetValueType()
+        {
+            return typeof(object);
+        }
+    }
+}
