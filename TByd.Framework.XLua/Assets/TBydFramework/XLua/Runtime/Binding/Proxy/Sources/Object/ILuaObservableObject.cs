7bc10ac05fc4e0ec06345ed685107b3252fe7f22
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:25:52 +0800

    Create ILuaObservableObject.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/Binding/Proxy/Sources/Object/ILuaObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using XLua;
+
+namespace TBydFramework.XLua.Runtime.Binding.Proxy.Sources.Object
+{
+    [CSharpCallLua]
+    public interface ILuaObservableObject
+    {
+        void subscribe(string key, Action action);
+
+        void unsubscribe(string key, Action action);
+
+        void subscribe(int key, Action action);
+
+        void unsubscribe(int key, Action action);
+    }
+}
