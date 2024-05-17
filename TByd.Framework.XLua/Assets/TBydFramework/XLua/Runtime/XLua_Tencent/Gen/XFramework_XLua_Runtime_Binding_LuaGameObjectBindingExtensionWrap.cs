e8ea69463d2dee82c23ad196169fcc30433c0773
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:58:43 +0800

    Create XFramework_XLua_Runtime_Binding_LuaGameObjectBindingExtensionWrap.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/XLua_Tencent/Gen/XFramework_XLua_Runtime_Binding_LuaGameObjectBindingExtensionWrap.cs
@@ -0,0 +1,81 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+using TBydFramework.XLua.Runtime.Binding;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class XFrameworkXLuaRuntimeBindingLuaGameObjectBindingExtensionWrap 
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			System.Type type = typeof(LuaGameObjectBindingExtension);
+			Utils.BeginObjectRegister(type, L, translator, 0, 0, 0, 0);
+			
+			
+			
+			
+			
+			
+			Utils.EndObjectRegister(type, L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(type, L, __CreateInstance, 1, 1, 0);
+			
+			
+            
+			Utils.RegisterFunc(L, Utils.CLS_GETTER_IDX, "Binder", _g_get_Binder);
+            
+			
+			
+			Utils.EndClassRegister(type, L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            return LuaAPI.luaL_error(L, "TBydFramework.XLua.Runtime.Binding.LuaGameObjectBindingExtension does not have a constructor!");
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _g_get_Binder(RealStatePtr L)
+        {
+		    try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			    translator.PushAny(L, LuaGameObjectBindingExtension.Binder);
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            return 1;
+        }
+        
+        
+        
+		
+		
+		
+		
+    }
+}
