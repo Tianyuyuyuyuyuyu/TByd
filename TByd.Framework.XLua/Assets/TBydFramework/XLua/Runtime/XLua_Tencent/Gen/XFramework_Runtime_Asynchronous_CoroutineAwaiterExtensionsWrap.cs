5298a34653b07534c52f29301d21541d38b789f2
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:53:57 +0800

    Create XFramework_Runtime_Asynchronous_CoroutineAwaiterExtensionsWrap.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/XLua_Tencent/Gen/XFramework_Runtime_Asynchronous_CoroutineAwaiterExtensionsWrap.cs
@@ -0,0 +1,110 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class XFrameworkRuntimeAsynchronousCoroutineAwaiterExtensionsWrap 
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			System.Type type = typeof(TBydFramework.Runtime.Asynchronous.CoroutineAwaiterExtensions);
+			Utils.BeginObjectRegister(type, L, translator, 0, 0, 0, 0);
+			
+			
+			
+			
+			
+			
+			Utils.EndObjectRegister(type, L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(type, L, __CreateInstance, 2, 0, 0);
+			Utils.RegisterFunc(L, Utils.CLS_IDX, "GetAwaiter", _m_GetAwaiter_xlua_st_);
+            
+			
+            
+			
+			
+			
+			Utils.EndClassRegister(type, L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            return LuaAPI.luaL_error(L, "TBydFramework.Runtime.Asynchronous.CoroutineAwaiterExtensions does not have a constructor!");
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_GetAwaiter_xlua_st_(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+            
+			    int gen_param_count = LuaAPI.lua_gettop(L);
+            
+                if(gen_param_count == 1&& translator.Assignable<System.Collections.IEnumerator>(L, 1)) 
+                {
+                    System.Collections.IEnumerator _coroutine = (System.Collections.IEnumerator)translator.GetObject(L, 1, typeof(System.Collections.IEnumerator));
+                    
+                        var gen_ret = TBydFramework.Runtime.Asynchronous.CoroutineAwaiterExtensions.GetAwaiter( _coroutine );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                if(gen_param_count == 1&& translator.Assignable<TBydFramework.Runtime.Asynchronous.IAsyncResult>(L, 1)) 
+                {
+                    TBydFramework.Runtime.Asynchronous.IAsyncResult _target = (TBydFramework.Runtime.Asynchronous.IAsyncResult)translator.GetObject(L, 1, typeof(TBydFramework.Runtime.Asynchronous.IAsyncResult));
+                    
+                        var gen_ret = TBydFramework.Runtime.Asynchronous.CoroutineAwaiterExtensions.GetAwaiter( _target );
+                        translator.PushAny(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+            return LuaAPI.luaL_error(L, "invalid arguments to TBydFramework.Runtime.Asynchronous.CoroutineAwaiterExtensions.GetAwaiter!");
+            
+        }
+        
+        
+        
+        
+        
+        
+		
+		
+		
+		
+    }
+}
