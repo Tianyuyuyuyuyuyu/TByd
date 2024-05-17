371e4277d885041da463e6671e0b0cc371556926
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-17 09:27:13 +0800

    Create LuaEnvironment.cs

---
+++ TByd.Framework.XLua/Assets/TBydFramework/XLua/Runtime/LuaEnvironment.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using TBydFramework.Runtime.Execution;
+using TBydFramework.Runtime.Log;
+using UnityEngine;
+using XLua;
+using IAsyncResult = TBydFramework.Runtime.Asynchronous.IAsyncResult;
+
+namespace TBydFramework.XLua.Runtime
+{
+    public static class LuaEnvironment
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(LuaEnvironment));
+
+        private static float interval = 2;
+        private static WaitForSecondsRealtime wait;
+        private static LuaEnv luaEnv;
+        private static IAsyncResult result;
+
+        public static float Interval
+        {
+            get { return interval; }
+            set
+            {
+                if (interval <= 0)
+                    return;
+
+                interval = value;
+                wait = new WaitForSecondsRealtime(interval);
+            }
+        }
+
+        public static LuaEnv LuaEnv
+        {
+            get
+            {
+                if (luaEnv == null)
+                {
+                    luaEnv = new LuaEnv();
+                    if (result != null)
+                        result.Cancel();
+
+                    wait = new WaitForSecondsRealtime(interval);
+                    result = Executors.RunOnCoroutine(DoTick());
+                }
+
+                return luaEnv;
+            }
+        }
+
+        public static void Dispose()
+        {
+            if (result != null)
+            {
+                result.Cancel();
+                result = null;
+            }
+
+            if (luaEnv != null)
+            {
+                luaEnv.Dispose();
+                luaEnv = null;
+            }
+
+            wait = null;
+        }
+
+        private static IEnumerator DoTick()
+        {
+            while (true)
+            {
+                yield return wait;
+                try
+                {
+                    luaEnv.Tick();
+                }
+                catch (Exception e)
+                {
+                    if (log.IsWarnEnabled)
+                        log.WarnFormat("LuaEnv.Tick Error:{0}", e);
+                }
+            }
+        }
+    }
+}
