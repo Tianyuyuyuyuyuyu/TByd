77a51f83eda05f695392b0f83326198643f8e4b8
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-28 20:18:45 +0800

    Update GameObjectPool.cs

--- TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/GameObjectPool.cs
+++ TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/GameObjectPool.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace uPools
+namespace TBydFramework.Module.Pool.Runtime
 {
     public sealed class GameObjectPool : IObjectPool<GameObject>
     {
