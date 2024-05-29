2c702c0d4857f44a0a1c0e0d52160e9236fe4020
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-29 20:39:35 +0800

    Update AbstractFrameworkModule.cs

--- TByd.Framework/Assets/TBydFramework/Runtime/Base/AbstractFrameworkModule.cs
+++ TByd.Framework/Assets/TBydFramework/Runtime/Base/AbstractFrameworkModule.cs
@@ -9,7 +9,7 @@
         /// 获取游戏框架模块优先级。
         /// </summary>
         /// <remarks>优先级较高的模块会优先轮询，并且关闭操作会后进行。</remarks>
-        internal virtual int Priority
+        public virtual int Priority
         {
             get
             {
@@ -22,11 +22,11 @@
         /// </summary>
         /// <param name="elapseSeconds">逻辑流逝时间，以秒为单位。</param>
         /// <param name="realElapseSeconds">真实流逝时间，以秒为单位。</param>
-        internal abstract void Update(float elapseSeconds, float realElapseSeconds);
+        public abstract void Update(float elapseSeconds, float realElapseSeconds);
 
         /// <summary>
         /// 关闭并清理游戏框架模块。
         /// </summary>
-        internal abstract void Shutdown();
+        public abstract void Shutdown();
     }
 }
