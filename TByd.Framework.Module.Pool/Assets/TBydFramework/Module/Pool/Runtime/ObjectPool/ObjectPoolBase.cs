24b17ca6be9892b58d0495f9315b1ed42d25e561
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-29 20:39:04 +0800

    Update ObjectPoolBase.cs

--- TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/ObjectPool/ObjectPoolBase.cs
+++ TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/ObjectPool/ObjectPoolBase.cs
@@ -1,11 +1,5 @@
-﻿//------------------------------------------------------------
-// Game Framework
-// Copyright © 2013-2021 Jiang Yin. All rights reserved.
-// Homepage: https://gameframework.cn/
-// Feedback: mailto:ellan@gameframework.cn
-//------------------------------------------------------------
-
-using System;
+﻿using System;
+using TBydFramework.Runtime.Base.DataStruct;
 
 namespace GameFramework.ObjectPool
 {
