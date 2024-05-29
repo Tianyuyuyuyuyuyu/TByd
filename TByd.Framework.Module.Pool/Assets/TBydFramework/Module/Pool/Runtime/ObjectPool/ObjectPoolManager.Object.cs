be3ad4bbd0c390d3b0633009fbd7c5d2ea4e9f51
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-05-29 20:14:39 +0800

    Update ObjectPoolManager.Object.cs

--- TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/ObjectPool/ObjectPoolManager.Object.cs
+++ TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/ObjectPool/ObjectPoolManager.Object.cs
@@ -9,7 +9,7 @@ using System;
 
 namespace GameFramework.ObjectPool
 {
-    internal sealed partial class ObjectPoolManager : GameFrameworkModule, IObjectPoolManager
+    internal sealed partial class ObjectPoolManager : AbstractFrameworkModule, IObjectPoolManager
     {
         /// <summary>
         /// 内部对象。
