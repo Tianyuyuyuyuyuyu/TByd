3b4b14356a5eb7739e92e2178b7bfc3777bfa570
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-06-03 16:47:45 +0800

    Create ILogHelper.cs

---
+++ TByd.Framework/Assets/TBydFramework/Runtime/Interface/ILogHelper.cs
@@ -0,0 +1,17 @@
+﻿using TBydFramework.Runtime.Enums;
+
+namespace TBydFramework.Runtime.Interface
+{
+    /// <summary>
+    /// 游戏框架日志辅助器接口。
+    /// </summary>
+    public interface ILogHelper
+    {
+        /// <summary>
+        /// 记录日志。
+        /// </summary>
+        /// <param name="level">游戏框架日志等级。</param>
+        /// <param name="message">日志内容。</param>
+        void Log(EnumLogLevel level, object message);
+    }
+}
