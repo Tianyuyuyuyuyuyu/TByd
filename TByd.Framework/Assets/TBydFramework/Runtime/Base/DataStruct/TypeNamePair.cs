c0cbf01f90f16b2ae7f2b617e31b1e229465bc63
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-06-03 16:29:52 +0800

    Update TypeNamePair.cs

--- TByd.Framework/Assets/TBydFramework/Runtime/Base/DataStruct/TypeNamePair.cs
+++ TByd.Framework/Assets/TBydFramework/Runtime/Base/DataStruct/TypeNamePair.cs
@@ -29,7 +29,7 @@ namespace TBydFramework.Runtime.Base.DataStruct
         {
             if (type == null)
             {
-                throw new GameFrameworkException("Type is invalid.");
+                throw new TBydFrameworkException("Type is invalid.");
             }
 
             m_Type = type;
@@ -66,7 +66,7 @@ namespace TBydFramework.Runtime.Base.DataStruct
         {
             if (m_Type == null)
             {
-                throw new GameFrameworkException("Type is invalid.");
+                throw new TBydFrameworkException("Type is invalid.");
             }
 
             string typeName = m_Type.FullName;
