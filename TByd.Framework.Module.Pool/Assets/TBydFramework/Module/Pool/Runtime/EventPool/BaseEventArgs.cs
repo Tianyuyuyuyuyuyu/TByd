1308d78c7a0bfe22ed19d6ec25b640e99d244504
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-06-03 16:24:45 +0800

    Update BaseEventArgs.cs

--- TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/EventPool/BaseEventArgs.cs
+++ TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/EventPool/BaseEventArgs.cs
@@ -1,11 +1,6 @@
-﻿//------------------------------------------------------------
-// Game Framework
-// Copyright © 2013-2021 Jiang Yin. All rights reserved.
-// Homepage: https://gameframework.cn/
-// Feedback: mailto:ellan@gameframework.cn
-//------------------------------------------------------------
+﻿using TBydFramework.Runtime.Base;
 
-namespace GameFramework
+namespace TBydFramework.Module.Pool.Runtime.EventPool
 {
     /// <summary>
     /// 事件基类。
