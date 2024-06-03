747ec0ae2d553eb07802fe7e1ac30f327aced9a4
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-06-03 11:35:26 +0800

    Update IAsyncObjectPool.cs

--- TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/External/UniTask/IAsyncObjectPool.cs
+++ TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/External/UniTask/IAsyncObjectPool.cs
@@ -2,7 +2,7 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 
-namespace TBydFramework.Module.Pool.Runtime
+namespace TBydFramework.Module.Pool.Runtime.External.UniTask
 {
     public interface IAsyncObjectPool<T>
     {
