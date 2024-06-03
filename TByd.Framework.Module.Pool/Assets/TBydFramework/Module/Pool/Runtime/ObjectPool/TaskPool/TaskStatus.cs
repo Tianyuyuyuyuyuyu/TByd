716c62dfecb0beb7f34a4672039331339b5dcbae
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-06-03 16:29:48 +0800

    Update TaskStatus.cs

--- TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/ObjectPool/TaskPool/TaskStatus.cs
+++ TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/ObjectPool/TaskPool/TaskStatus.cs
@@ -1,11 +1,4 @@
-﻿//------------------------------------------------------------
-// Game Framework
-// Copyright © 2013-2021 Jiang Yin. All rights reserved.
-// Homepage: https://gameframework.cn/
-// Feedback: mailto:ellan@gameframework.cn
-//------------------------------------------------------------
-
-namespace GameFramework
+﻿namespace TBydFramework.Module.Pool.Runtime.ObjectPool.TaskPool
 {
     /// <summary>
     /// 任务状态。
