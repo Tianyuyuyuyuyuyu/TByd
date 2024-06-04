6403d9bd4409c6dd937e6cb4e5bb94f37fecbcba
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-06-04 10:43:47 +0800

    Create IReference.cs

---
+++ TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/Base/Interface/IReference.cs
@@ -0,0 +1,13 @@
+﻿namespace TBydFramework.Module.Pool.Runtime.Base.Interface
+{
+    /// <summary>
+    /// 引用接口。
+    /// </summary>
+    public interface IReference
+    {
+        /// <summary>
+        /// 清理引用。
+        /// </summary>
+        void Clear();
+    }
+}
