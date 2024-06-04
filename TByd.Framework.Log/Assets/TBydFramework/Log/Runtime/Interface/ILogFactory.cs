f1fd059c339260c3b8966a16a071de728413b265
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-06-04 14:45:01 +0800

    Create ILogFactory.cs

---
+++ TByd.Framework.Log/Assets/TBydFramework/Log/Runtime/Interface/ILogFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TBydFramework.Log4Net.Runtime;
+
+namespace TBydFramework.Runtime.Log
+{
+    public interface ILogFactory
+    {
+        ILog GetLogger<T>();
+
+        ILog GetLogger(Type type);
+
+        ILog GetLogger(string name);
+    }
+}
