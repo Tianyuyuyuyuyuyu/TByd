f4453907d924bb536262e8c7a7ee9dd0bffea192
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-06-04 11:22:24 +0800

    Create PersistentMonoSingleton.cs

---
+++ TByd.Framework.Singleton/Assets/TBydFramework/Singleton/Runtime/PersistentMonoSingleton.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace TBydFramework.Singleton.Runtime
+{
+    /// <summary>
+    /// 如果跳转到新的场景里已经有了实例，则不创建新的单例（或者创建新的单例后会销毁掉新的单例）
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class PersistentMonoSingleton<T> : MonoBehaviour where T : Component
+    {
+        protected static T mInstance;
+        protected bool mEnabled;
+
+        /// <summary>
+        /// 单例设计模式
+        /// </summary>
+        /// <value>实例.</value>
+        public static T Instance
+        {
+            get
+            {
+                if (mInstance == null)
+                {
+                    mInstance = FindObjectOfType<T>();
+                    if (mInstance == null)
+                    {
+                        var obj = new GameObject();
+                        mInstance = obj.AddComponent<T>();
+                    }
+                }
+
+                return mInstance;
+            }
+        }
+
+        /// <summary>
+        /// Awake时，检查是否在场景中已经有一份对象的拷贝了，如果有就销毁它
+        /// </summary>
+        protected virtual void Awake()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            if (mInstance == null)
+            {
+                //如果自己是第一个实例，则将自己变成单例
+                mInstance = this as T;
+                DontDestroyOnLoad(transform.gameObject);
+                mEnabled = true;
+            }
+            else
+            {
+                //If a Singleton already exists and you find
+                //another reference in scene, destroy it!
+                if (this != mInstance)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+}
