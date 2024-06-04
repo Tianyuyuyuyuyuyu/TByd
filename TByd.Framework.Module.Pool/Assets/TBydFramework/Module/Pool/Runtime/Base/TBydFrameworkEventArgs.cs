2b53fa8a0e15189516f1d7a1c7c98c389815d6aa
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-06-04 10:43:56 +0800

    Create TBydFrameworkEventArgs.cs

---
+++ TByd.Framework.Module.Pool/Assets/TBydFramework/Module/Pool/Runtime/Base/TBydFrameworkEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TBydFramework.Module.Pool.Runtime.Base.Interface;
+
+namespace TBydFramework.Module.Pool.Runtime.Base
+{
+    /// <summary>
+    /// 游戏框架中包含事件数据的类的基类。
+    /// </summary>
+    public abstract class TBydFrameworkEventArgs : EventArgs, IReference
+    {
+        /// <summary>
+        /// 初始化游戏框架中包含事件数据的类的新实例。
+        /// </summary>
+        public TBydFrameworkEventArgs()
+        {
+        }
+
+        /// <summary>
+        /// 清理引用。
+        /// </summary>
+        public abstract void Clear();
+    }
+}
