02e6f60d7bf71e15fd7139eb7ef94764ae1a37d7
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-24 21:36:04 +0800

    fairy gui core

---
+++ TByd.Framework.FairyGUI/Assets/TBydFramework/FairyGUI/Runtime/Core/Stats.cs
@@ -0,0 +1,29 @@
+﻿
+namespace TBydFramework.FairyGUI.Runtime.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Stats
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static int ObjectCount;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static int GraphicsCount;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static int LatestObjectCreation;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static int LatestGraphicsCreation;
+    }
+}
