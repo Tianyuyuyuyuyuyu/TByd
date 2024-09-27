daf5bf69ce7ca1d03e1d20c669012b88289e7fd0
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-27 11:54:35 +0800

    Create TerminationNode.cs

---
+++ 需要研究/reanimation-master/Runtime/Nodes/TerminationNode.cs
@@ -0,0 +1,9 @@
+﻿using Aarthificial.Reanimation.Cels;
+
+namespace Aarthificial.Reanimation.Nodes
+{
+    public abstract class TerminationNode : ReanimatorNode
+    {
+        public abstract ICel ResolveCel(IReadOnlyReanimatorState previousState, ReanimatorState nextState);
+    }
+}
