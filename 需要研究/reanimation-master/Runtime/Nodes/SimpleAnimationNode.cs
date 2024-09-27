51562199b9cc630d9a28ade550ea75610bcf6b8d
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-27 11:54:03 +0800

    Create SimpleAnimationNode.cs

---
+++ 需要研究/reanimation-master/Runtime/Nodes/SimpleAnimationNode.cs
@@ -0,0 +1,10 @@
+﻿using Aarthificial.Reanimation.Cels;
+using UnityEngine;
+
+namespace Aarthificial.Reanimation.Nodes
+{
+    [CreateAssetMenu(fileName = "simple_animation", menuName = "Reanimator/Simple Animation", order = 400)]
+    public class SimpleAnimationNode : AnimationNode<SimpleCel>
+    {
+    }
+}
