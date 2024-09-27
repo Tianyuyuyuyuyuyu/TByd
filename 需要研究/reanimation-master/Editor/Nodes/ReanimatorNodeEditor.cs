c899031f26609ef85d834eb3e1419dc8e83bc02e
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-27 11:48:20 +0800

    Create ReanimatorNodeEditor.cs

---
+++ 需要研究/reanimation-master/Editor/Nodes/ReanimatorNodeEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+namespace Aarthificial.Reanimation.Editor.Nodes
+{
+    public abstract class ReanimatorNodeEditor : UnityEditor.Editor
+    {
+        private readonly List<SerializedProperty> _propertiesToDraw = new List<SerializedProperty>();
+
+        protected SerializedProperty AddCustomProperty(string propertyName)
+        {
+            var property = serializedObject.FindProperty(propertyName);
+            _propertiesToDraw.Add(property);
+            return property;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawCustomProperties();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        protected void DrawCustomProperties()
+        {
+            foreach (var property in _propertiesToDraw)
+            {
+                EditorGUILayout.Separator();
+                EditorGUILayout.PropertyField(property);
+            }
+        }
+    }
+}
