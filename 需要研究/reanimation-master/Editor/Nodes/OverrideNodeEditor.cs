ccb690d05221a54f72acf0be84f4df5009addecd
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-27 11:48:04 +0800

    Create OverrideNodeEditor.cs

---
+++ 需要研究/reanimation-master/Editor/Nodes/OverrideNodeEditor.cs
@@ -0,0 +1,28 @@
+﻿using Aarthificial.Reanimation.Nodes;
+using UnityEditor;
+
+namespace Aarthificial.Reanimation.Editor.Nodes
+{
+    [CustomEditor(typeof(OverrideNode))]
+    public class OverrideNodeEditor : UnityEditor.Editor
+    {
+        private SerializedProperty _next;
+        private SerializedProperty _override;
+
+        private void OnEnable()
+        {
+            _next = serializedObject.FindProperty("next");
+            _override = serializedObject.FindProperty("overrides");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(_next);
+            EditorGUILayout.PropertyField(_override);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
