e46ffe99fdce28659b613b1836ff209937a91ea8
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-27 11:53:08 +0800

    Create MirroredAnimationNode.cs

---
+++ 需要研究/reanimation-master/Runtime/Nodes/MirroredAnimationNode.cs
@@ -0,0 +1,10 @@
+﻿using Aarthificial.Reanimation.Cels;
+using UnityEngine;
+
+namespace Aarthificial.Reanimation.Nodes
+{
+    [CreateAssetMenu(fileName = "mirrored_animation", menuName = "Reanimator/Mirrored Animation", order = 400)]
+    public class MirroredAnimationNode : AnimationNode<MirroredCel>
+    {
+    }
+}
