c48f0b515a5e3b89ae70760f40352b69bc55f987
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-27 11:53:40 +0800

    Create ReanimatorNode.cs

---
+++ 需要研究/reanimation-master/Runtime/Nodes/ReanimatorNode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Aarthificial.Reanimation.Nodes
+{
+    public abstract class ReanimatorNode : ScriptableObject
+    {
+        public abstract TerminationNode Resolve(IReadOnlyReanimatorState previousState, ReanimatorState nextState);
+
+        protected void AddTrace(ReanimatorState nextState)
+        {
+#if UNITY_EDITOR
+            nextState.AddTrace(this);
+#endif
+        }
+    }
+}
