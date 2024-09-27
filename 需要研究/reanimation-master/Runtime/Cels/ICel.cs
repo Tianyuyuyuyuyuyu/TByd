b31d70fe07d48c517f1da03b81a59499ae9c6139
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-27 11:51:00 +0800

    Create ICel.cs

---
+++ 需要研究/reanimation-master/Runtime/Cels/ICel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Aarthificial.Reanimation.Cels
+{
+    public interface ICel
+    {
+        void ApplyToRenderer(
+            ReanimatorState previousState,
+            ReanimatorState nextState,
+            SpriteRenderer renderer
+        );
+    }
+}
