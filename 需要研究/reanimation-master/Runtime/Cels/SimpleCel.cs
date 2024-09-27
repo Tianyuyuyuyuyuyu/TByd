bd8e770a4bf3bbcb4d7eeb1f4280c7319024a2fc
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-27 11:51:48 +0800

    Create SimpleCel.cs

---
+++ 需要研究/reanimation-master/Runtime/Cels/SimpleCel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Aarthificial.Reanimation.Common;
+using UnityEngine;
+
+namespace Aarthificial.Reanimation.Cels
+{
+    [Serializable]
+    public class SimpleCel : ICel
+    {
+        [SerializeField] protected Sprite sprite;
+        [SerializeField] protected DriverDictionary drivers = new DriverDictionary();
+
+        public SimpleCel()
+        {
+        }
+
+        public SimpleCel(Sprite sprite = null, DriverDictionary drivers = null)
+        {
+            if (sprite != null)
+                this.sprite = sprite;
+            if (drivers != null)
+                this.drivers = drivers;
+        }
+
+        public virtual void ApplyToRenderer(
+            ReanimatorState previousState,
+            ReanimatorState nextState,
+            SpriteRenderer renderer
+        )
+        {
+            nextState.Merge(drivers);
+            renderer.sprite = sprite;
+            renderer.flipX = previousState.ShouldFlip();
+        }
+    }
+}
