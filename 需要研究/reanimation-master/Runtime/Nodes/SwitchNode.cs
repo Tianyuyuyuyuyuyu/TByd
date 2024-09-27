dd42f7f8e5c447304911e385302336ecae28cab9
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-27 11:54:19 +0800

    Create SwitchNode.cs

---
+++ 需要研究/reanimation-master/Runtime/Nodes/SwitchNode.cs
@@ -0,0 +1,21 @@
+﻿using Aarthificial.Reanimation.Common;
+using UnityEngine;
+
+namespace Aarthificial.Reanimation.Nodes
+{
+    [CreateAssetMenu(fileName = "switch", menuName = "Reanimator/Switch", order = 400)]
+    public class SwitchNode : ReanimatorNode
+    {
+        [SerializeField] protected ReanimatorNode[] nodes;
+        [SerializeField] protected ControlDriver controlDriver = new ControlDriver();
+        [SerializeField] protected DriverDictionary drivers = new DriverDictionary();
+
+        public override TerminationNode Resolve(IReadOnlyReanimatorState previousState, ReanimatorState nextState)
+        {
+            AddTrace(nextState);
+            nextState.Merge(drivers);
+            return nodes[controlDriver.ResolveDriver(previousState, nextState, nodes.Length)]
+                .Resolve(previousState, nextState);
+        }
+    }
+}
