63cf026e0b24374c8bda8993c1704c093d72fd6f
CLEVELAND <34181960+Tianyuyuyuyuyuyu@users.noreply.github.com>
2024-09-27 11:51:32 +0800

    Create MirroredCel.cs

---
+++ 需要研究/reanimation-master/Runtime/Cels/MirroredCel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Aarthificial.Reanimation.Common;
+using UnityEngine;
+
+namespace Aarthificial.Reanimation.Cels
+{
+    [Serializable]
+    public class MirroredCel : ICel
+    {
+        [SerializeField] protected Sprite sprite;
+        [SerializeField] protected DriverDictionary drivers = new DriverDictionary();
+        [SerializeField] private Sprite spriteLeft;
+
+        public void ApplyToRenderer(
+            ReanimatorState previousState,
+            ReanimatorState nextState,
+            SpriteRenderer renderer
+        )
+        {
+            nextState.Merge(drivers);
+            renderer.sprite = previousState.ShouldFlip() ? spriteLeft : sprite;
+            renderer.flipX = false;
+        }
+    }
+}
